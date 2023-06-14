a95a1ab36fb8afd5e072f794d942ce585c24867f
JIUNG9 <60885635+JIUNG9@users.noreply.github.com>
2023-06-15 08:05:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.27 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,61 @@
 import java.util.*;
 
 class Solution {
-    
-    static int xLen;
-    static int yLen;
-    static int[][] visited;
-    static int dx[] ={1,0,-1,0};
-    static int dy[] ={0,1,0,-1};
+    int dx[] = {1,0,-1,0};
+    int dy[] = {0,1,0,-1};
+    static int row;
+    static int col;
+    static int visited[][];
     
     public int solution(int[][] maps) {
-     //visited
-        xLen = maps.length;
-        yLen = maps[0].length;
-        visited = new int[xLen][yLen];
-        bfs(maps,visited);
-        int ans = visited[xLen-1][yLen-1];
-        if(ans == 0){
+    
+        
+        row = maps.length;
+        col = maps[0].length;
+        visited = new int[row][col];
+        bfs(maps);
+        //도달하지 못 하는 경우
+        if(visited[row-1][col-1] == 0){
             return -1;
         }
-        return ans;
-        
+        //도달하는 경우
+        return visited[row-1][col-1];
     }
     
-    public void bfs(int[][] maps, int[][] visited){
-        int x = 0;
-        int y = 0;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x,y});
-        visited[x][y] = 1;
-        
-        while(!queue.isEmpty()){
-            int[] current = queue.remove();
-            int cX = current[0];
-            int cY = current[1];
-            
-            for(int i = 0; i < 4; i++){
-                int nX = cX + dx[i];
-                int nY = cY + dy[i];
-                
-                if(nX < 0 || nY < 0 || nX > maps.length-1 || nY > maps[0].length-1 ) continue;
-                
-                if(visited[nX][nY] == 0 && maps[nX][nY] == 1){
-                    visited[nX][nY] = visited[cX][cY] + 1;
-                    queue.add(new int[]{nX,nY});
-                }
-            }
-            
+    public void bfs(int[][] m) {
+
+    int x = 0;
+    int y = 0;
+    Queue<int[]> q = new LinkedList<>();
+    q.add(new int[]{x, y});
+    visited[x][y] = 1;
+    //진행
+    while (!q.isEmpty()) {
+      int[] current = q.remove();
+      int cX = current[0];
+      int cY = current[1];
+
+      //현재 위치에서 nX, nY를 체크하면서 Queue를 진행
+      for (int i = 0; i < 4; i++) {
+        int nX = cX + dx[i];
+        int nY = cY + dy[i];
+        //방문하지 못 하는 경우 (사이즈가 넘은 경우, 이미 방문한 경우, 방문 예정인 곳이 0인 경우)
+        //ArrayIndexOutOfBoundsException => If I check range with the place which I can go => the error will be trhown
+
+        if (nX < 0 || nX > row-1 || nY < 0 || nY > col-1) {
+          continue;
         }
-        
-    
-        
+        //방문하는 경우(상, 하, 좌, 우 체크), 가중치 추가
+        if (visited[nX][nY] == 0 && m[nX][nY] == 1) {
+          q.add(new int[]{nX, nY});
+          visited[nX][nY] = visited[cX][cY] + 1;
+        }
+      }
     }
+        
+        
+        
+        
+        
+}
 }
