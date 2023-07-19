ca4c20921d26aa9c2e3107b5dda5884a12ed9df3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2023-07-19 23:59:16 +0900

    [Gold V] Title: Australian Voting, Time: 180 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Gold/4419. Australian Voting/Australian Voting.java
+++ 백준/Gold/4419. Australian Voting/Australian Voting.java
@@ -1,122 +1,107 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-  public static HashMap<String, Integer> candidates = new HashMap<>();
-  public static int n;
+    private static final int MAX_CANDIDATES = 20;
+    private static final int MAX_VOTERS = 1000;
 
-  public static void main(String args[]) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    String input = "";
+    private static final Map<String, Integer> candidateVotes = new HashMap<>();
+    private static int numCandidates;
 
-    n = Integer.parseInt(br.readLine());
-    ArrayList<String> nameList = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> candidateNames = new ArrayList<>();
+        int[][] votes = new int[MAX_VOTERS][MAX_CANDIDATES];
+        String input;
+        int voterCount = 0;
 
-    // 17 ~ 40 initialize
-    for (int i = 0; i < n; i++) {
-      String name = br.readLine();
-      candidates.put(name, 0);
-      nameList.add(name);
-    }
-
-    int votes[][] = new int[1000][1000];
-
-    int idx = 0;
-    while ((input = br.readLine()) != null) {
-      StringTokenizer st = new StringTokenizer(input);
-
-      for (int i = 0; i < n; i++)
-        votes[idx][i] = Integer.parseInt(st.nextToken());
-
-      idx++;
-
-    }
-
-    for (int i = 0; i < idx; i++) {
-      String name = nameList.get(votes[i][0] - 1);
-      candidates.put(name, candidates.getOrDefault(name, 0) + 1);
-    }
-
-    while (true) {
-      // 44 ~ 45 투표순으로 후보자 오름차순 정렬
-      List<Entry<String, Integer>> entryList = new LinkedList<>(candidates.entrySet());
-      entryList.sort(Map.Entry.comparingByValue());
-
-      // 최다 득표수의 백분율
-      double max = (entryList.get(entryList.size() - 1).getValue() / (double) idx) * 100;
-
-      int minValue = entryList.get(0).getValue();
-      int maxValue = entryList.get(entryList.size() - 1).getValue();
-
-      // 최다 득표수가 과반 수 이거나 최대 값과 최소 값이 같으면 후보자들을 출력하고 종료 54 ~ 69
-      if (max > 50 || minValue == maxValue) {
-        Stack<String> st = new Stack<>();
-
-        // 최대 득표 후보가 여러명 있을 수 있으므로 모두 출력
-        for (int i = entryList.size() - 1; i >= 0; i--) {
-          st.push(entryList.get(i).getKey());
-
-          if (i == 0 || entryList.get(i).getValue() != entryList.get(i - 1).getValue())
-            break;
+        numCandidates = Integer.parseInt(reader.readLine());
+        if (numCandidates > MAX_CANDIDATES) {
+            throw new IOException("후보자 수가 " + MAX_CANDIDATES + "명보다 많습니다. " + MAX_CANDIDATES + "명 이하로 입력하세요");
         }
 
-        // entryList를 맨 끝에서 부터 탐색 했기 때문에 거꾸로 다시 출력
-        while (!st.isEmpty())
-          System.out.println(st.pop());
-
-        break;
-      }
-
-      // 최소 득표 후보자들을 제거 (최소 득표 후보자들이 여러명 있을 수 있으므로 모두 제거) (72 ~ 83)
-      for (int i = 0; i < entryList.size() - 1; i++, idx--) {
-        String name = entryList.get(i).getKey();
-        int index = nameList.indexOf(name);
-
-        entryList.remove(0);
-        candidates.remove(name);
-
-        // 최소 득표자 제거 후 다음 순위 후보자에게 표를 넘겨줌
-        findNextVote(votes, index + 1, nameList, minValue);
-
-        if (minValue != entryList.get(0).getValue())
-          break;
-      }
-    }
-
-  }
-
-  public static void findNextVote(int[][] votes, int idx, ArrayList<String> nameList,
-      int minValue) {
-
-    for (int i = 0; i < votes.length; i++) {
-
-      if (votes[i][0] != idx)
-        continue;
-
-      for (int j = 1; j < n; j++) {
-        String name = nameList.get(votes[i][j] - 1);
-
-        if (candidates.containsKey(name)) {
-          if (minValue < candidates.get(name))
-            candidates.put(name, candidates.get(name) + 1);
-          break;
+        for (int i = 0; i < numCandidates; i++) {
+            String name = reader.readLine();
+            if (name.length() > 80) {
+                throw new IOException("후보자 이름이 80글자 이하로 작성하시오");
+            }
+            candidateVotes.put(name, 0);
+            candidateNames.add(name);
         }
-      }
-    }
-  }
 
+        while ((input = reader.readLine()) != null && input.length() > 0) {
+            StringTokenizer tokenizer = new StringTokenizer(input);
+            if (voterCount >= MAX_VOTERS) {
+                throw new IOException("최대 투표 가능 인원수는 " + MAX_VOTERS + "명입니다.");
+            }
+            for (int i = 0; i < numCandidates; i++) {
+                votes[voterCount][i] = Integer.parseInt(tokenizer.nextToken());
+            }
+            voterCount++;
+        }
+
+        for (int i = 0; i < voterCount; i++) {
+            String name = candidateNames.get(votes[i][0] - 1);
+            candidateVotes.put(name, candidateVotes.getOrDefault(name, 0) + 1);
+        }
+
+        while (true) {
+            List<Map.Entry<String, Integer>> candidateList = new ArrayList<>(candidateVotes.entrySet());
+            candidateList.sort(Map.Entry.comparingByValue());
+            double maxPercentage = (candidateList.get(candidateList.size() - 1).getValue() / (double) voterCount) * 100;
+            int minValue = candidateList.get(0).getValue();
+            int maxValue = candidateList.get(candidateList.size() - 1).getValue();
+
+            if (maxPercentage > 50 || minValue == maxValue) {
+                Stack<String> winnerStack = new Stack<>();
+                for (int i = candidateList.size() - 1; i >= 0; i--) {
+                    winnerStack.push(candidateList.get(i).getKey());
+
+                    if (i == 0 || !candidateList.get(i).getValue().equals(candidateList.get(i - 1).getValue())) {
+                        break;
+                    }
+                }
+                while (!winnerStack.isEmpty()) {
+                    System.out.println(winnerStack.pop());
+                }
+                break;
+            }
+
+            for (int i = 0; i < candidateList.size() - 1; i++, voterCount--) {
+                String name = candidateList.get(i).getKey();
+                int index = candidateNames.indexOf(name);
+
+                candidateList.remove(0);
+                candidateVotes.remove(name);
+
+                updateNextVote(votes, index + 1, candidateNames, minValue);
+
+                if (!candidateList.isEmpty() && minValue != candidateList.get(0).getValue()) {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void updateNextVote(int[][] votes, int idx, List<String> candidateNames, int min) {
+        for (int[] vote : votes) {
+            if (vote[0] != idx) {
+                continue;
+            }
+
+            for (int j = 1; j < numCandidates; j++) {
+                String name = candidateNames.get(vote[j] - 1);
+
+                if (candidateVotes.containsKey(name)) {
+                    if (min < candidateVotes.get(name)) {
+                        candidateVotes.put(name, candidateVotes.get(name) + 1);
+                    }
+                    break;
+                }
+            }
+        }
+    }
 }
-
