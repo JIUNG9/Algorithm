54f4355aa71e8c9dc19f1f1f9e5e60f889a034a2
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-15 10:48:45 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.16 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,40 +1,36 @@
-
-import java.util.*;
-import java.util.stream.Collectors;
-
 class Solution {
-   public String solution(String s) {
-    //1.1 if the charAt(0) is the number do not change
-    //1.2 else, change the charAt(0) to upper case
-    //2. if charAt(i) is lowercase and i - 1 is equal to " ", change i to uppercase
-    //3. if chartAt(i) is the uppercase and i - 1 is not equal to " " change i to lower case 
-    StringBuilder sb = new StringBuilder();
-
-
-
-    //These cases are the 1.1 and 1.2
-    int len = s.length();
-    if (s.charAt(0) >= 97 && s.charAt(0) <= 122) {
-      sb.append(Character.toString(s.charAt(0) - 32));
+    public String solution(String s) {
+        //s의 길이가 200 문자열 -> 띄어쓰기로만해도 200개 -> n^2을 해도 괜찮다.
+        //숫자는 단어의 첫 문자로만 나옵니다. -> 매번 숫자인지 아닌지 확인, 숫자가 아니라면 upperCase
+        //구현 방법: 공백 기준으로 문자열을 나눈 후 첫 문자가 숫자가 아니라 영어라면 upper case로 수정
+        //이후 하나의 문자열로 합치기
+        
+        
+        String answer = "";
+        s = s.toLowerCase();
+        
+        for(int i = 1; i < s.length(); i++){
+            boolean flag = false;
+            while(s.charAt(i) ==' ' && i < s.length()-1){
+                    answer = answer.concat(" ");                        
+                    i++;
+                flag = true;
+                
+            }
+            
+            if(flag){
+                answer = answer.concat(String.valueOf(Character.toUpperCase(s.charAt(i))));
+            }
+            else{
+                answer = answer.concat(String.valueOf(s.charAt(i)));
+                
+            }
+            
+            
+        }
+        
+        
+        return String.valueOf(s.charAt(0)).toUpperCase() + answer;
+        
     }
-    else{
-      sb.append(s.charAt(0));
-    }
-
-
-    //These cases are 2, 3
-    for (int i = 1; i < len; i++) {
-
-    if (s.charAt(i) >= 97 && s.charAt(i) <= 122 && s.charAt(i-1)==' ') {
-      sb.append(Character.toString(s.charAt(i)-32));
-    }
-    else if (s.charAt(i) >= 62 && s.charAt(i) <= 90 && s.charAt(i-1)!=' ') {
-      sb.append(Character.toString(s.charAt(i)+32));
-    }
-    else{
-      sb.append(s.charAt(i));
-    }
-    }
-return sb.toString();
-  }
 }
