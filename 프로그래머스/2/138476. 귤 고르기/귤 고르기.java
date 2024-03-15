aa733ae6678847b75d61f2b1449865d986e184d6
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-15 16:19:23 +0900

    [level 2] Title: 귤 고르기, Time: 60.60 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,42 +1,24 @@
-import java.util.Arrays;
-
-
+import java.util.*;
+import java.util.stream.*;
 class Solution {
- public int solution(int k, int[] tangerine) {
-    int len = tangerine.length;
-    int[] weight = new int[len];
-    int j = 0;
-    int sum = 0;
-    int counter = 1;
-    int answer = 1;
-    Arrays.sort(tangerine);
-
-    weight[j] = 1;
-
-    //get weights with same element
-    for (int i = 0; i < len - 1; ) {
-      if (tangerine[i] == tangerine[i + 1]) {
-        weight[j] = ++counter;
-      } else {
-        j += 1;
-        counter = 1;
-        weight[j] = 1;
-      }
-      i++;
+    public int solution(int k, int[] t) {
+        
+       //각 귤의 개수를 개수를 파악한 이후 같은 크기의 가장 많은 귤의 개수에서 k를 뺀다.(역순 정렬) 이것을 반복하고 k가 0보다 작아진다면 반복한 횟수를 리턴한다.
+        
+        int counter =0;
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i =0; i < t.length; i++){
+            map.put(t[i],map.getOrDefault(t[i],0)+1);
+        }
+        
+        List<Integer> valueList = new ArrayList<>(map.values());
+        valueList = valueList.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        for(int i = 0; i < valueList.size(); i++){
+            if(k > 0) {
+                k-=valueList.get(i);    
+                counter++;
+            }
+        }
+        return counter;
     }
-
-    Arrays.sort(weight);
-
-    for (int i = len-1; i >= 0; i--) {
-      sum += weight[i];
-      if (sum >= k) {
-        return answer;
-      }
-      else{
-        answer++;
-      }
-    }
-    return -1;
-  }
-
 }
