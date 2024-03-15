0f706d53960001e793e3264b4dc200265286b790
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-15 15:15:41 +0900

    [level 2] Title: 구명보트, Time: 9.20 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,33 +1,42 @@
-import java.util.Arrays;
-
-
+import java.util.*;
 class Solution {
-  public int solution(int[] people, int limit) {
-    int len = people.length;
-    int left = 0;
-    int right = len-1;
-    int counter = 0;
-    Arrays.sort(people);
+    public int solution(int[] p, int l) {
+        //무게를 정렬
+        //가장 무거운 사람을 담아본다. -> 무거운 사람이 limit보다 작다면 가벼운 사람을 계속해서 추가한다.
+        //추가하다가 한계를 넘으면 보트를 하나 추가하고 보낸다.
+        //만약 그 합이 한계보다 작으면 그 다음 가벼운 사람을 될 때까지 싣는다. 초과했다면 보트를 하나 추가한다. 
+        
+        //구현 : two pointer
+        Arrays.sort(p);
 
-    while(left <= right){
-      if(people[left] >= limit) {
-        left++;
-        counter++;
-      }
-      else if (people[left] + people[right] <= limit){
-        counter++;
-        left++;
-        right--;
-      }
-      else if(people[left] + people[right] > limit)
-      {
-        counter++;
-        right--;
-      }
+        int light = 0;
+        int heavy = p.length-1;
+        int sum = p[heavy];
+        int counter = 0;
+        
+        while(light <= heavy){     
+            
+            sum= p[light] + p[heavy];
+        
+            
+            if(sum > l){
+                sum-=p[heavy--];
+                counter++;
+                
+            }
+            else{
+            heavy--;
+            light++;
+            counter++;
+            }
+            
+     
+            
+            
+            
+        
+            }
+        return counter;
+            }
+            
     }
-      //when break the loop by left index of array.
-      //between left and right elements need boat for each person
-      return counter;
-
-    }
-}
