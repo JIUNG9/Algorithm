534f934197dc3665d336054d41ccdf456848d710
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-19 11:31:20 +0900

    [level 2] Title: 소수 찾기, Time: 1.91 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,59 +1,79 @@
-
 import java.util.*;
-
 class Solution {
- private final int MAX_VALUE = 9999999;
+    
+    private boolean[ ] visited;
+    private Set<Integer> set;
+   /*
+   numbers는 길이 1 이상 7 이하인 문자열입니다.
+ -> 완전탐색, DFS
+ 
+ 
+ visited, Set<Integer> (중복제거)
+ 011 -> Integer.parse로 해결 -> 11로 변환
+ 
+ 문제 접근: 숫자를 하나씩 concat하면서 만약 소수라면 정답을 하나씩 추가
+ 구현: 
+ //맨 앞자리를 바꿔가면서 dfs를 호출
+ for(int i = 0; i < len; i++){
+ visited = new visited[length]; -> 새로운 visited arr 만들기
+ visited[i] = true;
+ dfs()
+ }
+ 
+ //dfs -> 방문하지 않았고 Set이 포함하지 않았고 해당 숫자가 프라임 넘버라면 ->Set에추가
+ //Set에 추가 -> Integer.parseInt()
+ //visited 방문 처리 
+ // dfs(String.concat()) 을 사용
+ 
+ //dfs 종료조건: iteration 중 len와 동일해진다면 break
+ 
+ 
+   */
+    public int solution(String s) {
+        
+        int len = s.length();
+        set = new HashSet<>();
+        
 
-  public int solution(String numbers) {
-    Set<Integer> set = new HashSet<>();
-    //1. add every cases
-    permutation("", numbers, set);
-    //2. filter the set
-    filterPrime(set);
-    //3. return
-    return set.size();
-  }
-
-
-  public boolean[] getPrimeArr() {
-    boolean[] isPrime = new boolean[MAX_VALUE + 1];
-    Arrays.fill(isPrime, true);
-
-    isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i * i <= MAX_VALUE; i++) {
-      if (isPrime[i]) {
-        for (int j = i * i; j <= MAX_VALUE; j += i) {
-          isPrime[j] = false;
+        visited = new boolean[len];
+        dfs("",len, s,visited );
+        
+        return set.size();
+    }
+    
+    
+    public void dfs(String str, int len, String number, boolean[] visited) {
+        
+        
+        if(!str.equals("") && isPrime(Integer.parseInt(str))){
+            set.add(Integer.parseInt(str));
+        }  
+        
+        for(int i =0; i < len; i++){
+            if(str.length() < len && !visited[i]){
+                visited[i] = true;
+                dfs(str.concat(number.substring(i,i+1)), len, number, visited);
+                visited[i] = false;
+            }
+            
         }
-      }
+    
+        
+        
     }
-    return isPrime;
-  }
+        
+            
+        
 
-
-  public void filterPrime(Set<Integer> set) {
-    boolean[] primeChecker = getPrimeArr();
-    set.remove(0);
-    set.remove(1);
-
-    for (int i = 2; i < MAX_VALUE; i++) {
-      if (!primeChecker[i]) {
-        set.remove(i);
-      }
+    
+    private static boolean isPrime(int num) {
+        if(num <= 1)
+            return false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
-  }
-
-
-  public void permutation(String s, String pattern, Set<Integer> set) {
-
-    if (!s.equals("")) {
-      set.add(Integer.valueOf(s));
-    }
-    for (int i = 0; i < pattern.length(); i++) {
-      permutation(s + pattern.charAt(i), pattern.substring(0, i) + pattern.substring(i + 1), set);
-
-    }
-
-
-  }
 }
