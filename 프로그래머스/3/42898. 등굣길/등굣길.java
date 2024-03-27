80da3c4143baf42104b08ad79480396fd1314053
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-27 15:38:55 +0900

    [level 3] Title: 등굣길, Time: 0.91 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,28 +1,27 @@
-import java.util.*;
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int [][] map = new int[n][m];
-        
-        if(puddles[0].length > 0){
-            for(int x[] : puddles){
-                map[x[1]-1][x[0]-1] = -1;
-            }
+        int answer = 0;
+        int[][] map = new int[n][m];
+        for (int[] puddle : puddles) {
+            map[puddle[1]-1][puddle[0]-1] = -1;
         }
-        for(int i = 1 ;i < n; i++){
-            map[i][0] = (map[i][0] == -1 | map[i-1][0] == -1) ? -1 : 1;
-        }
-        for(int j = 1 ;j < m ;j++){
-            map[0][j] = (map[0][j] == -1 | map[0][j-1] == -1) ? -1 : 1;
-        }
-        for(int i =1;i < n; i++){
-            for(int j= 1 ;j < m; j++){
-                if(map[i][j] != -1){
-                    int a = map[i-1][j] == -1 ? 0 : map[i-1][j];
-                    int b = map[i][j-1] == -1 ? 0 : map[i][j-1];   
-                    map[i][j] = (a+b) == 0 ? -1 : (a+b) % 1000000007;   
+        map[0][0] = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] != -1) {
+                    if (i >= 1 && map[i - 1][j] != -1) {
+                        map[i][j] += map[i - 1][j];
+                    }
+                    if (j >= 1 && map[i][j - 1] != -1) {
+                        map[i][j] += map[i][j - 1];
+                    }
+                    if (map[i][j] > 1000000007) {
+                        map[i][j] %= 1000000007;
+                    }
                 }
             }
         }
-        return map[n-1][m-1] == -1 ? 0 : map[n-1][m-1];
+        answer = map[n-1][m-1];
+        return answer;
     }
 }
