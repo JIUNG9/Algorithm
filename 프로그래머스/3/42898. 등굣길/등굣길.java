f7ae7fdb636a4952f87ca6ec90bfa0fb3dfc7bc8
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-28 14:02:37 +0900

    [level 3] Title: 등굣길, Time: 0.51 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,20 +1,30 @@
-class Solution {
+   class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<puddles.length;i++){
-            dp[puddles[i][0]][puddles[i][1]]=-1;
-        }
-        dp[1][1]=1; 
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(dp[i][j]==-1){
-                    dp[i][j]=0;
+        int[][] map= new int[n+1][m+1];
+        map[1][1] = 1; //starting point
+        map= puddle(map, puddles);
+        return dp(map, m, n);
+    }
+
+    public int[][] puddle(int[][] map, int[][] puddles){
+        for(int i=0; i<puddles.length;i++)
+            map[puddles[i][1]][puddles[i][0]]= -1;
+        return map;
+    }
+
+    public int dp(int[][] map, int m, int n){
+        for (int j=1; j<=n; j++){
+            for(int k=1; k<=m;k++){
+                if (j == 1 && k == 1)       //(1,1)은 1로 만들어두고, 0이 되지 않도록
                     continue;
-                }
-                if(i!=1)    dp[i][j]+=dp[i-1][j]%1000000007;
-                if(j!=1)    dp[i][j]+=dp[i][j-1]%1000000007;
+                if (map[j][k] == -1){ //웅덩이는 0으로 만들어 다음 덧셈 때 영향끼치지 않게
+                     map[j][k] = 0;
+                    continue;
+                }         
+            map[j][k] = (map[j][k-1] + map[j-1][k]) %1000000007;
+                //[a,b] = [a-1,b] + [a,b-1] 공식
             }
         }
-        return dp[m][n]%1000000007;
+        return map[n][m]; // result
     }
 }
