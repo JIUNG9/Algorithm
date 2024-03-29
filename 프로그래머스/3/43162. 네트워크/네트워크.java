20c3cca7279576de97ce28b4c3a8e575053a4517
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-03-29 15:59:55 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,88 +1,59 @@
+import java.util.*;
 class Solution {
-
+    private int answer = 0;
     private boolean[][] visited;
-    
-   //1번과 2번이 연결되어있을 때 2번과 연결된 모든 노드들도 연결되어있어 1개라고 표시하면서
-    //이미 방문한 것을 제외한 2번 3번 4번 n 번까지 반복하여 연결된 노드의 수를 구한다.
-    
-    
-    // 구현:
-    /*
-    1번을 먼저 시작하고 자기 자신을 제외한 것을 순회해야한다. 자기 자신은 항상 computers 1 처리가 되어있다.
-    방문을 하지 않은 것을 방문해야한다. 1과 2를 방문 했다는 것은 visited[i][j] [j][i] 동일하다 
-    
-    visited를 dfs에 파라미터로 하지 않는다. 스택 x
-    
-    
-    for(int i =0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(i==j) visited[i][j] = true;
-        }
-    }
-    
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(c[i][j]==1 && !visited[i][j] && visited[j][i]) {
-            dfs(c,j,i);
-            counter++;
-        }
-    }
-    
-    //1과 2가 연결되어있다고 할 때 2와 연결된 모든 노드들을 1과 방문 처리
-    public void dfs(int[][] c, int node, int connectedNode){
-    
-    for(int i = 0; i < c.length; i++){
-        if(!visited[node][i] && !visited[i][node]){
-            visited[connectedNode][i] = true;
-            visited[i][connectedNode] = true;
-            dfs(c,node1,node2)
-        }
-    }
-    
-    }
-    
-    
-    
-    */
     public int solution(int n, int[][] c) {
         
-        visited = new boolean[n][n];
-        int counter = 0;
-        int standAloneCounter = 0;
-    for(int i =0; i < n; i++){
-        boolean standAloneFlag = true;
-        for(int j = 0; j < n; j++){
-            if(i==j) visited[i][j] = true;
-            else if(c[i][j] != 0){
-                standAloneFlag = false;
+        int len = c.length;
+        visited = new boolean[len][len];
+        
+        
+        for(int i = 0; i < len; i++){
+            for(int j = 0; j < len; j++){
+                if(i==j) continue;
+                if(!visited[i][j] && !visited[j][i]&& c[i][j] == 1 &&  c[j][i] == 1){
+                    visited[i][j] = true;
+                    visited[j][i] = true;
+                    dfs(c,j,i);
+                    answer++;
+                
             }
         }
-        if(standAloneFlag) standAloneCounter++;
+        }
+
+            return answer+getNotConnectedNodeCount(visited);
+    
+    }
+        
+        
+            
+        
+
+    
+    public int getNotConnectedNodeCount(boolean[][] visited){
+        int counter = 0;
+        for(boolean[] boolArr: visited){
+            boolean flag = false;
+            for(boolean b: boolArr){
+                if(b) flag = true;
+            }
+            if(!flag) counter++;
+        }
+        return counter;
     }
     
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(c[i][j]==1 && !visited[i][j]) {
-                System.out.println("before start dfs");
-                System.out.println("i, j" + i + " "+ j);
-            dfs(c,j,i);
-            counter++;
+    
+    public void dfs(int[][] c, int targetNode, int originNode){
+        int len = c.length;
+        for(int i = 0; i < len; i++){
+            if(i==targetNode) continue;
+            if(c[targetNode][i] == 1 && c[i][targetNode] == 1 && !visited[targetNode][i] && !visited[i][targetNode]){
+                visited[i][targetNode] = true;
+                visited[targetNode][i] = true;
+                dfs(c,i,targetNode);
+            }
+            
+            }
         }
-    }
-    }
-        
-        return counter + standAloneCounter;
-    }
-        
-    public void dfs(int[][] c , int node, int connectedNode){
-            for(int i = 0; i < c.length; i++){
-        if(!visited[node][i] && c[node][i] == 1){
-            visited[node][i] = true;
-            visited[i][node] = true;
-            visited[connectedNode][i] = true;
-            visited[i][connectedNode] = true;
-            dfs(c,i,connectedNode);
-        }
-    }
-    }
+    
 }
