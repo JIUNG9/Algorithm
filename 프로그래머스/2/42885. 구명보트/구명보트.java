7d1fd97edafa281c654a28bfea875c35426293d3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-04-02 06:25:44 +0900

    [level 2] Title: 구명보트, Time: 9.32 ms, Memory: 54.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,42 +1,31 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] p, int l) {
-        //무게를 정렬
-        //가장 무거운 사람을 담아본다. -> 무거운 사람이 limit보다 작다면 가벼운 사람을 계속해서 추가한다.
-        //추가하다가 한계를 넘으면 보트를 하나 추가하고 보낸다.
-        //만약 그 합이 한계보다 작으면 그 다음 가벼운 사람을 될 때까지 싣는다. 초과했다면 보트를 하나 추가한다. 
+    public int solution(int[] people, int limit) {
+        int boat = 0;
+        int len = people.length;
+        //1명인 경우 return 1;
+        if(len==1) return 1;
         
-        //구현 : two pointer
-        Arrays.sort(p);
-
-        int light = 0;
-        int heavy = p.length-1;
-        int sum = p[heavy];
-        int counter = 0;
+        Arrays.sort(people);
         
-        while(light <= heavy){     
-            
-            sum= p[light] + p[heavy];
         
-            
-            if(sum > l){
-                sum-=p[heavy--];
-                counter++;
-                
+        int lightIdx = 0;
+        int heavyIdx = len -1;
+        
+        while (lightIdx <= heavyIdx){
+            if(people[lightIdx] + people[heavyIdx] <= limit){
+                boat++;
+                lightIdx++;
+                heavyIdx--;
             }
             else{
-            heavy--;
-            light++;
-            counter++;
+                boat++;
+                heavyIdx--;
             }
+        }
+        return boat;
             
-     
             
-            
-            
-        
-            }
-        return counter;
-            }
             
     }
+}
