e43ed4367b38d06062730f056f1b71957b8f262b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-04-04 16:29:27 +0900

    [level 3] Title: 등굣길, Time: 0.50 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,22 +1,48 @@
+import java.util.*;
 class Solution {
-   public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<puddles.length;i++){
-            dp[puddles[i][0]][puddles[i][1]]=-1;
+    public int solution(int m, int n, int[][] puddles) {
+        
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] isPuddle = new boolean[n+1][m+1];
+        
+        
+        for(int[] p : puddles){
+            isPuddle[p[1]][p[0]] = true;
         }
-        dp[1][1]=1; 
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(dp[i][j]==-1){
-                    dp[i][j]=0;
-                    continue;
-                }
-                if(i==1 && j == 1){
-                    continue;
-                }    
-                dp[i][j]= (dp[i-1][j] + dp[i][j-1]) % 1000000007; 
+        
+        // for(int i = 1; i <= n; i++){
+        //     for(int j = 1; j <= m; j++){
+        //         System.out.println("boolean: "+ isPuddle[i][j]);
+        //     }
+        // }
+        
+        for(int i = 1; i <= n; i++){
+            if(isPuddle[i][1]) break;
+            dp[i][1] = 1;
+        }
+        
+        for(int j = 1; j <= m; j++){
+            if(isPuddle[1][j]) break;
+            dp[1][j] = 1;
+        }
+        
+        
+        for(int i = 1; i <=n; i++){
+            for(int j = 1; j <= m; j++){
+                if(i == 1 && j ==1) continue;
+                if(isPuddle[i][j]){
+                    dp[i][j] = 0;
+                } 
+                else{
+                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) %1000000007;
             }
+                    // System.out.println("dp[" + i + "]" + "[" + j+"] : "+ dp[i][j]);
+
         }
-        return dp[m][n];
+        }
+        return dp[n][m];
+        
+        
+        
     }
 }
