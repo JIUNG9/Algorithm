29fe8e5ebdb680f100148ef4979c429022a33d4c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-04-04 11:40:38 +0900

    [level 2] Title: 땅따먹기, Time: 25.86 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,37 +1,42 @@
 import java.util.*;
 
 class Solution {
-      public int solution(int[][] land) {
-      //DP[i][j]는 DP[i-1][j]를 제외한 DP[i-1]의 행의 가장 큰 값과 land[i][j]의 값으로 이뤄진다.
-    int len = land.length;
-    int width = land[0].length;
-    int[][] dp = initDp(land);
-
-    for(int i = 1; i < len; i++){
-      for(int j = 0; j < width; j++){
-        dp[i][j] = land[i][j] + getMaxValueWithoutInputIdx(dp[i-1], j);
-      }
-    }
-    return getMaxValueWithoutInputIdx(dp[len-1], Integer.MAX_VALUE);
-  }
-
-  private int getMaxValueWithoutInputIdx(int[] arr, int idx) {
-    int len = arr.length;
-    int max = 0;
-    for(int i = 0; i < len; i++){
-      if(i == idx) continue;
-      max = Math.max(max,arr[i]);
-    }
-    return max;
-  }
-
-  public int[][] initDp(int[][] land){
-    int width = land.length;
-    int len = land[0].length;
-    int[][] dp = new int[width][len];
-        for(int i =0; i < len; i++){
-          dp[0][i] = land[0][i];
+    
+    
+    public int findMaxExcludeInputIdx(int[] arr, int idx){
+        int max = idx == 0 ? arr[1] : arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(max < arr[i] && idx != i) max = arr[i];                
         }
-    return dp;
-  }
+        return max;
+        
+    }
+    
+    int solution(int[][] land) {
+        int len = land.length;
+        int[][] dp = new int[len][4];
+        
+        dp[0][0] = land[0][0];
+        dp[0][1] = land[0][1];
+        dp[0][2] = land[0][2];
+        dp[0][3] = land[0][3];
+
+        for(int i = 1; i < len; i++){
+            for(int j = 0; j < 4; j++){
+                int max = findMaxExcludeInputIdx(dp[i-1],j);
+                dp[i][j] = land[i][j] + max;
+            }
+        }
+        
+        int answer = dp[len-1][0];
+        System.out.println("answer: "+ answer);
+        for(int k = 0; k < 4; k++){
+            if(answer < dp[len-1][k]){
+                answer = dp[len-1][k];
+            }
+        }
+        return answer;
+        
+
+    }
 }
