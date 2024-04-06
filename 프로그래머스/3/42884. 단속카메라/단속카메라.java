dcba33813ec3ddcf4cc71e041820c0280ca3cf9b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-04-06 11:18:06 +0900

    [level 3] Title: 단속카메라, Time: 24.65 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,31 +1,29 @@
 import java.util.*;
-import java.util.stream.*;
 class Solution {
     public int solution(int[][] r) {
-
-        int answer = 0;
         Arrays.sort(r,(o1,o2)->{
             return Integer.compare(o1[1],o2[1]);
         });
+        
+        
+        
+        int counter = 1;
+        int len = r.length;
+        int camPosition = r[0][1];
+        for(int i = 1; i < len; i++){
+            // System.out.println("r[" + i + "][0]: " + r[i][0]);
+            // System.out.println("r[" + i + "][1]: " + r[i][1]);
 
-        
-        
-        int pointer = r[0][1];
-        for(int i = 1; i < r.length; i++){
-            if(pointer < r[i][0]){
-                pointer = r[i][1];
-                answer++;
-                // System.out.println("answer:" + answer);
-            }
+            if(r[i][0] > camPosition){
+                counter++;
+                camPosition = r[i][1];
+            } 
+            
+            
+            
         }
+        return counter;
         
-        
-
-        return answer+1;
         
     }
 }
-
-
-
-
