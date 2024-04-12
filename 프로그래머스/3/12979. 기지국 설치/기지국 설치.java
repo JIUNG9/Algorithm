a2050778e3ddb4b126b807deb487617f861bdd59
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-04-12 10:36:07 +0900

    [level 3] Title: 기지국 설치, Time: 0.90 ms, Memory: 53.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -0,0 +1,23 @@
+class Solution {
+ 
+public int solution(int n, int[] stations, int w) {
+        int answer = 0;
+        int s=0;
+        int left =1;
+        while(true){
+        if(s < stations.length && left >= stations[s] - w){
+            left = stations[s]+w+1;
+            s++;            
+        }
+        else{     
+            left=left+w+w+1;
+            answer++;
+        }
+        if(left> n){
+            break;
+        }
+    }
+
+        return answer;
+    }
+}
