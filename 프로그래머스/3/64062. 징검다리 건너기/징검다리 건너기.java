d96443bf424823e79a9a3e2ec88af54848c916f4
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-03 09:20:56 +0900

    [level 3] Title: 징검다리 건너기, Time: 32.31 ms, Memory: 65.2 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -1,33 +1,44 @@
-import java.util.*;
-class Data{
-   int value, index;
-   public Data(int value, int index) {
-      this.value = value;
-      this.index = index;
-   }
-}
 class Solution {
+      private int answer = Integer.MIN_VALUE;
+  public int solution(int[] stones, int k) {
+   // mid 값을 순차적으로 뺏을 때 0 이하의 숫자가 연속적으로 K개 이상 나탔을 때
+   // 해당 숫자는 건널 수 없다. -> 값을 줄인다. -> r = mid -1;
+   // 만약 해당 숫자가 건널 수 있다. -> 값을 키운다.  l = mid + 1;
 
-   public static int solution(int[] stones, int k) {
-      int answer = Integer.MAX_VALUE;
+    int len = stones.length;
+    int l = 0;
+    int r = -1;
+    int max = Integer.MIN_VALUE;
+    for(Integer i : stones){
+      if(i > max) max = i;
+    }
 
-      int n = stones.length;
-      ArrayDeque<Data> q = new ArrayDeque<>();
+    r = max;
 
-      for(int i = 0; i < n; i++) {
-         int cur = stones[i];
-         if(!q.isEmpty() && q.peek().index <= i - k) {
-            q.pollFirst();
-         }
-         while(!q.isEmpty() && q.peekLast().value <= cur) {
-            q.pollLast();
-         }
-         q.add(new Data(cur, i));
-         if(answer >= q.peekFirst().value && i >= k - 1) {
-            answer = q.peekFirst().value;
-         }
+    while(l <= r){
+      int mid = (l+r)/2;
+      int counter = 0;
+      int maxCounter = 0;
+      for(int i = 0; i < len; i++){
+        if(stones[i] - mid < 0){
+          counter++;
+          maxCounter = Math.max(maxCounter,counter);
+        }
+        else{
+          counter = 0;
+        }
+      }
+      //건널 수 없다.
+      if(maxCounter >= k){
+        r = mid-1;
+      }
+      else{
+        answer = Math.max(answer,mid);
+        l = mid + 1;
       }
 
-      return answer;
-   }
+    }
+
+    return answer;
+}
 }
