18c6a5d1c72142429977d07e4f7f9888b25b742f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-17 06:30:02 +0900

    [level 2] Title: 택배상자, Time: 332.04 ms, Memory: 225 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,44 +1,127 @@
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 class Solution {
+   public int solution(int[] order) {
+    SupplementBelt supplementBelt = new SupplementBelt();
+    MainBelt mainBelt = new MainBelt(1000001);
+    Loaded loaded = new Loaded();
+    int i = 0;
+    int currentShouldBeLoad = order[0];
 
-  public int solution(int[] o) {
-    int len = o.length;
-    int idx = 0;
+    while (mainBelt.isThereBox()) {
 
-    //보조
-    Stack<Integer> stk = new Stack<>();
-    //container
-    List<Integer> container = Arrays.stream(o).boxed().sorted().collect(Collectors.toList());
+        while (mainBelt.isThereBox() && mainBelt.currentBox() <= currentShouldBeLoad) {
+          supplementBelt.input(mainBelt.out());
+        }
 
-
-    for (int i = 0; i < len; i++) {
-      boolean flag = false;
-      if (o[idx] == container.get(i)) {
-        idx++;
-        flag = true;
-      }
-      while (!stk.isEmpty() && stk.peek() == o[idx]) {
-                stk.pop();
-                idx++;
-                flag = true;
-            }
-       if(!flag) {
-        stk.push(container.get(i));
-      }
-    }
-
-      for (int j = idx; j < len; ) {
-        if (!stk.isEmpty() && stk.peek() == o[idx]) {
-          stk.pop();
-          idx++;
-        } else {
-          break;
+      while (supplementBelt.isThereBox() && supplementBelt.getRecentBox() == currentShouldBeLoad) {
+        loaded.load(supplementBelt.out());
+        i++;
+        if (i < order.length) {
+          currentShouldBeLoad = order[i];
         }
       }
-    return idx;
+      if(mainBelt.currentBox() > currentShouldBeLoad){
+        if(supplementBelt.isThereBox()){
+          if(supplementBelt.getRecentBox() != currentShouldBeLoad) break;
+        }
+        else break;
+      }
+
+}
+    return loaded.totalLoaded();
+
+
+  }
+
+
+  class Loaded {
+
+    private final List<Integer> list;
+
+    public Loaded() {
+      this.list = new ArrayList<>();
+    }
+
+    public void load(int value) {
+      list.add(value);
+    }
+
+    public int totalLoaded() {
+      return list.size();
+
+    }
+
+  }
+
+
+  class MainBelt {
+
+    Queue<Integer> q;
+
+    public MainBelt(int size) {
+      init(size);
+    }
+
+    private void init(int size) {
+      q = new LinkedList<>();
+      for (int i = 1; i <= size; i++) {
+        q.add(i);
+      }
+    }
+
+    public int out() {
+      return q.poll();
+    }
+
+    public boolean isThereBox() {
+      return !q.isEmpty();
+    }
+
+    public int currentBox() {
+      return q.peek();
     }
 
 
+  }
+
+  class SupplementBelt {
+
+    private Stack<Integer> stk;
+
+    public SupplementBelt() {
+      init();
+    }
+
+    private void init() {
+      stk = new Stack<>();
+
+    }
+
+    public boolean isThereBox() {
+      return !stk.isEmpty();
+    }
+
+    public void input(int box) {
+      if (stk == null) {
+        init();
+      }
+      stk.add(box);
+    }
+
+    public int out() {
+      return stk.pop();
+    }
+
+    public int getRecentBox() {
+      return stk.peek();
+    }
+
+  }
+
+    
 }
