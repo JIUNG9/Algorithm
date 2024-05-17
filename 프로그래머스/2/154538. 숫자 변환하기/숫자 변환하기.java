901a9893832567746aca870026982950cece3d45
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-17 17:48:31 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.77 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,70 +1,26 @@
 import java.util.*;
-
 class Solution {
-  public int solution(int x, int y, int n) {
-        Queue<xStatus> q = new LinkedList<>();
-        Set<Integer> s = new HashSet<>();
-        initQueue(q,x);
-        int result = bfs(q,x,y,n,s);
-     System.out.println(result);
-        return result;
-
-
-
-    }
-    public void initQueue(Queue<xStatus> q, int x){
-        q.add(new xStatus(x,0));
-    }
-
-
-    public int bfs(Queue<xStatus> q, int x, int y, int n, Set<Integer> set){
-
-
-        while(!q.isEmpty()){
-
-            xStatus status = q.poll();
-            int currentX = status.getX();
-            int currentCounter = status.getCounter();
-
-        if(currentX==y) {
-            return currentCounter;
-
-        }
-            else if(currentX < y && !set.contains(currentX)){
-                if(currentX * 3 <= y) {
-                    q.add(new xStatus(currentX *3 , currentCounter+1));
-                    set.add(currentX);
+    private int answer = -1;
+    Queue<int[]> q;
+    
+    public int solution(int x, int y, int n) {
+        q = new LinkedList<>();
+        q.add(new int[]{y, 0});
+        
+            while(!q.isEmpty()){
+                int[] arr  = q.poll();
+                int value = arr[0];
+                int counter = arr[1];
+                if(value == x) {
+                    answer = counter;
+                    break;
                 }
-                if(currentX * 2 <= y) {
-                    q.add(new xStatus(currentX *2 , currentCounter+1));
-                    set.add(currentX);
-
-                }
-                if(currentX + n <= y) {
-                    q.add(new xStatus(currentX + n , currentCounter+1));
-                    set.add(currentX);
-
-                }
-            }
-
-        }
-
-        return -1;
+                if(value - n > 0) q.add(new int[]{value-n,counter+1});
+                if(value % 2 == 0 && value / 2 > 0) q.add(new int[]{value /2,counter+1});
+                if(value % 3 == 0 && value / 3 > 0) q.add(new int[]{value / 3, counter+1});
+            }  
+        return answer;
+        
     }
-    class xStatus{
-        private final int x;
-        private final int counter;
 
-        public xStatus(int x, int counter){
-            this.x = x;
-            this.counter = counter;
-        }
-
-        public int getX(){
-            return this.x;
-        }
-        public int getCounter(){
-            return this.counter;
-        }
-    }
 }
