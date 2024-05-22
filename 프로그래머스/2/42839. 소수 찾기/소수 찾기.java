a1cfbffde639fb39a678881e0798bcc18a8a997f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-22 09:32:51 +0900

    [level 2] Title: 소수 찾기, Time: 3.70 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,77 +1,43 @@
 import java.util.*;
 class Solution {
-    
-    private boolean[ ] visited;
     private Set<Integer> set;
-   /*
-   numbers는 길이 1 이상 7 이하인 문자열입니다.
- -> 완전탐색, DFS
- 
- 
- visited, Set<Integer> (중복제거)
- 011 -> Integer.parse로 해결 -> 11로 변환
- 
- 문제 접근: 숫자를 하나씩 concat하면서 만약 소수라면 정답을 하나씩 추가
- 구현: 
- //맨 앞자리를 바꿔가면서 dfs를 호출
- for(int i = 0; i < len; i++){
- visited = new visited[length]; -> 새로운 visited arr 만들기
- visited[i] = true;
- dfs()
- }
- 
- //dfs -> 방문하지 않았고 Set이 포함하지 않았고 해당 숫자가 프라임 넘버라면 ->Set에추가
- //Set에 추가 -> Integer.parseInt()
- //visited 방문 처리 
- // dfs(String.concat()) 을 사용
- 
- //dfs 종료조건: iteration 중 len와 동일해진다면 break
- 
- 
-   */
-    public int solution(String s) {
-        
-        int len = s.length();
+    private boolean[] used;
+    public int solution(String numbers) {
         set = new HashSet<>();
-        
-
-        visited = new boolean[len];
-        dfs("",len, s,visited );
-        
+        String[] arr = numbers.split("");
+        used = new boolean[arr.length];
+        dfs("",numbers);
         return set.size();
+        
+        
+        
     }
-    
-    
-    public void dfs(String str, int len, String number, boolean[] visited) {
+    public void dfs(String pat,String numbers){
         
+        if(!pat.equals("") && isPrime(Integer.parseInt(pat))) set.add(Integer.parseInt(pat));
         
-        if(!str.equals("") && isPrime(Integer.parseInt(str))){
-            set.add(Integer.parseInt(str));
-        }  
-        
-        for(int i =0; i < len; i++){
-            if(str.length() < len && !visited[i]){
-                visited[i] = true;
-                dfs(str.concat(number.substring(i,i+1)), len, number, visited);
-                visited[i] = false;
+        for(int i = 0; i < numbers.length(); i++){
+            if(pat.length() < numbers.length() && !used[i]){
+            used[i] = true;
+            dfs(pat + numbers.substring(i,i+1),numbers);
+            used[i] = false;
             }
-            
         }
-    
         
         
     }
-        
-            
-    
-    
-    public boolean isPrime(int n){
-        if(n==0) return false;
-        if(n==1) return false;
-        
-        for(int i = 2; i < n; i++){
-            if(n%i == 0) return false;
+public boolean isPrime(int n) {
+    if (n <= 1) return false; 
+    if (n == 2) return true;  
+    if (n % 2 == 0) return false; 
+
+    // Check for factors from 3 to sqrt(n)
+    int sqrt = (int) Math.sqrt(n);
+    for (int i = 3; i <= sqrt; i += 2) {
+        if (n % i == 0) {
+            return false; 
         }
-        return true;
     }
+    return true; 
+}
 }
