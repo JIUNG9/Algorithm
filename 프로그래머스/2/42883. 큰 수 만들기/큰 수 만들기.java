ad38cc65f9adc7e57affe041b909650bfef9a319
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-28 11:19:14 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.23 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,25 +1,30 @@
 import java.util.*;
 class Solution {
-    public String solution(String number, int k) {
-     
-        int answerLen = number.length() - k;
-        
-        Stack<Character> stk = new Stack<>();
-        
-        
-        for(int i =0; i< number.length(); i++){
-                while(!stk.isEmpty() && number.charAt(i) > stk.peek() && k > 0){
-                    stk.pop();
-                    k--;
-                }
-                stk.push(number.charAt(i));
-            }
-        
-        
-        String answer ="";
-        for(int j = 0; j < answerLen; j++){
-            answer = answer.concat(String.valueOf(stk.get(j)));
-        }
-        return answer;
+ public String solution(String number, int k) {
+    StringBuilder sb = new StringBuilder();
+    int len = number.length();
+    Stack<Integer> stk = new Stack<>();
+    stk.push(Integer.parseInt(number.substring(0, 1)));
+    int removeCounter = 0;
+
+    for (int i = 1; i < len; i++) {
+      int currValue = Integer.parseInt(number.substring(i, i + 1));
+
+      if(removeCounter == k) {
+        stk.push(currValue);
+        continue;
+      }
+
+      while (!stk.isEmpty() && stk.peek() < currValue && removeCounter < k) {
+        stk.pop();
+        removeCounter++;
+      }
+      stk.push(currValue);
     }
+   while(!stk.isEmpty()){
+      sb.append(String.valueOf(stk.pop()));
+   }
+   return sb.reverse().substring(0,len-k);
+
+  }
 }
