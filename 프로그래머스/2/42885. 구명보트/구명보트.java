60337f9bec250d1bd52e8f4a0764805a11129e56
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-28 15:19:04 +0900

    [level 2] Title: 구명보트, Time: 8.95 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,31 +1,31 @@
 import java.util.*;
+
 class Solution {
+    private int boat;
     public int solution(int[] people, int limit) {
-        int boat = 0;
-        int len = people.length;
-        //1명인 경우 return 1;
-        if(len==1) return 1;
+        
+        //증명-> 가장 무거운 사람과 가장 가벼운 사람을 짝지어 태웠을 때 그 다음 무거운 사람과 그 다음 가벼운 사람의 조합 또한 같거나 더 가벼운 이유. -> 가장 무거운 사람과 가장 가벼운 사람이 limit보다 작다면 가장 작은 사람 무게의 사람보다 더 무거운 사람의 사람을 찾아 최적의 해를 구하지 않아도 되는 이유는, 가장 가벼운 사람과 가장 무거운 사람의 합을 더하지 않는다면 이후 가장 가벼운 사람과 그 다음 무거운 사람의 조합을 찾게 되는데, 가장 가벼운 사람의 무게와 그 다음 무거운 사람의 합은 항상 가장 무거운 사람과 가장 가벼운 사람의 무게의 합보다 작기 때문이다.
         
         Arrays.sort(people);
-        
-        
-        int lightIdx = 0;
-        int heavyIdx = len -1;
-        
-        while (lightIdx <= heavyIdx){
-            if(people[lightIdx] + people[heavyIdx] <= limit){
+        int l  = 0;
+        int r = people.length - 1;
+        while(l <= r){
+            if(l == r) {
+                boat+=1;
+                break;
+            }
+            else if(people[l] + people[r] <= limit){
+                l++;
+                r--;
                 boat++;
-                lightIdx++;
-                heavyIdx--;
             }
             else{
+                r--;
                 boat++;
-                heavyIdx--;
             }
         }
+        
         return boat;
-            
-            
-            
+        
     }
 }
