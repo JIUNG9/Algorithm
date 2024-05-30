e826554fad348f671ca0bd59a055072d06fd2bf3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-05-30 10:51:29 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 254.73 ms, Memory: 271 MB -BaekjoonHub

--- 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -1,56 +1,80 @@
 import java.util.*;
+
 class Solution {
-    
-      public String addPadding(String s){
-    return "0"+s;
-    }
+    private List<Long> cache;
+    private long[] answer;
     public long[] solution(long[] numbers) {
         int len = numbers.length;
-        int answerIdx = 0;
-        long[] answer = new long[len];
+        int idx = 0;
+        answer = new long[len];
+        
+        initCache();
         for(int i = 0; i < len; i++){
             long num = numbers[i];
-            String strBinary = converToBinary(num);
-            strBinary = addPadding(strBinary);
-            int strLen = strBinary.length();
-            long number =0 ;
-            int idx = 1;            
-            int digit = 0;
+            String strNum = convertToString(num);
+            String reversed = new StringBuilder(strNum).reverse().toString();
+            
+            for(int j = 0; j < reversed.length(); j++){
+                
+                if(reversed.substring(j, j + 1).equals("0")) {
+                    num+=cache.get(j);
+                    for(int k = j-1; k >= 0; k--){
+                        if(reversed.substring(k,k+1).equals("1")){
+                            num-=cache.get(k);
+                            break;
 
-            char c = strBinary.charAt(strLen-idx);    
-            
-            while(c != '0'){
-                digit++;
-                idx++;
-                if(strLen-idx < 0) break;
-                c = strBinary.charAt(strLen-idx);
-            }
-          if (digit != 0) {
-                number = num + getDigit(digit) - getDigit(digit-1);
-            } else {
-                number = num + getDigit(digit);
+                        }
+                    }
+                    break;
+                }
             }
             
-            answer[answerIdx++] = number;
+            answer[idx++] = num;
+        
         }
+        
+       
+        
         return answer;
     }
     
-    public long getDigit(long iteration){
-        long digit = 1;
-        while(iteration > 0){
-            digit *=2;
-            iteration--;
-        }
-        return digit;
-    }
+
     
-    public String converToBinary(long num){
+    private void initCache(){
+        cache = new ArrayList<>();
+        cache.add(1L);
+        int idx = 0;
+        while(idx < 47){
+            long recent = cache.get(idx);
+            cache.add(recent * 2L);
+            idx++;
+        }
+    }
+ 
+    
+    private String convertToString(long num){
         StringBuilder sb = new StringBuilder();
         while(num > 0){
             sb.append(String.valueOf(num%2));
             num/=2;
         }
+        sb.append("0");
         return sb.reverse().toString();
-}
+        
+    }
+    private long covnertToLong(String num){
+        long result = 0L;
+        StringBuilder sb = new StringBuilder(num);
+        num = sb.reverse().toString();
+        for(int i = 0 ;i < num.length(); i++){
+            if(num.substring(i, i+1).equals("1")){
+                result+=cache.get(i);
+            }
+        }
+                    return result;
+
+    }
+    
+    
+    
 }
