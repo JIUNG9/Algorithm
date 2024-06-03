a5accd70e741bcc3ea394f86ed36a27d4bfef27d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-03 11:37:17 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.89 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,71 +1,93 @@
-import java.util.*;
+import java.util.PriorityQueue;
+    
 class Solution {
-   public int solution(int bl, int weight, int[] tr) {
-    Queue<Truck> waitedQ = new LinkedList<>();
-    Queue<Truck> movingQ = new LinkedList<>();
-    int answer = 0;
-    int totalW = 0;
-    for (int i : tr) {
-      waitedQ.add(new Truck(i));
-    }
-    //기다리는 트럭이 존재하지 않을 때 까지 반복
-    //한 번에 하나의 트럭씩 진입
-    //만약 현재의 무게가 weight보다 작다면 다리에 한 번 더 적재, 또한 bl보다 현재 다리에 있는 트럭 +1 이 작거나 같아야함, 또한 대기 중인 트럭이 존재
-    //무빙 큐에 있는 것들은 하나씩 무빙, 그리고 이미 도달했다면 삭제
 
-    while (!waitedQ.isEmpty() || !movingQ.isEmpty()) {
-      answer++;
+     private int secondCounter = 0;
+    
+  public int solution(int bridge_length, int weight, int[] truck_weights) {
+    Bridge bridge = new Bridge(bridge_length, weight);
+    int idx = 0;
 
-      //움직이는 것이 없다면 하나를 대기 중인 트럭을 가져오기
-      if (movingQ.isEmpty()) {
-        Truck t = waitedQ.poll();
-        totalW += t.getWeight();
-        movingQ.add(t);
-          continue;
+    while (idx < truck_weights.length) {
+      Truck newTruck = new Truck(truck_weights[idx]);
+      if (bridge.canLoad(newTruck)) {
+        bridge.load(newTruck);
+        idx++;
       }
+      secondCounter++;
+      bridge.move();
+      bridge.unload();
 
-      //다리에 있는 트럭  움직이기
-        for (Truck t : movingQ) {
-          t.moving();
-        }
-      //다리를 지난 트럭 내리기
-         if (movingQ.peek().move > bl) {
-                Truck t = movingQ.poll();
-                totalW -= t.weight;
-            }
-        
-      //다리에 올릴 수 있는 트럭 체크하고 올리기
-      if (!waitedQ.isEmpty() && totalW + waitedQ.peek().getWeight() <= weight && movingQ.size()+1 <= bl) {
-        Truck t = waitedQ.poll();
-        totalW += t.getWeight();
-        movingQ.add(t);
-      }
     }
-    return answer;
+
+    while (!bridge.isBridgeEmpty()) {
+      secondCounter++;
+      bridge.move();
+      bridge.unload();
+    }
+    return secondCounter+1;
+
   }
-
 
   class Truck {
 
     int weight;
-    int move;
+    int movedTime;
+
 
     public Truck(int weight) {
       this.weight = weight;
-      move = 1;
+      this.movedTime = 0;
     }
 
-    public void moving() {
-      this.move++;
+    public void move() {
+      this.movedTime += 1;
+    }
+  }
+
+  class Bridge {
+
+    int currentWeight;
+    int tolerableWeight;
+    int length;
+    PriorityQueue<Truck> truckQ;
+
+
+    public Bridge(int length, int tolerableWeight) {
+      this.tolerableWeight = tolerableWeight;
+      this.length = length;
+      this.truckQ = new PriorityQueue<>((t1, t2) -> {
+        return Integer.compare(t2.movedTime, t1.movedTime);
+      });
+      this.currentWeight = 0;
     }
 
-    public int getWeight() {
-      return this.weight;
+    public boolean canLoad(Truck newTruck) {
+      return currentWeight + newTruck.weight <= tolerableWeight;
     }
 
-    public int getMove() {
-      return this.move;
+    public boolean isBridgeEmpty() {
+      return this.truckQ.isEmpty();
 
     }
+
+    public void load(Truck newTruck) {
+      if (currentWeight + newTruck.weight <= tolerableWeight) {
+        truckQ.add(newTruck);
+        currentWeight += newTruck.weight;
+      }
+
+    }
+
+    public void move() {
+      truckQ.forEach(Truck::move);
+    }
+
+    public void unload() {
+      while (!truckQ.isEmpty() && truckQ.peek().movedTime >= length) {
+        this.currentWeight -= truckQ.remove().weight;
+      }
+    }
+
   }
 }
