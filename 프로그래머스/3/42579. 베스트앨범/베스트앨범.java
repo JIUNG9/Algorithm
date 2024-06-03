abf879242e69648e8c016fe78c26944f9adf6c76
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-04 06:31:56 +0900

    [level 3] Title: 베스트앨범, Time: 4.86 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -3,56 +3,49 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.PriorityQueue;
+import java.util.Set;
 import java.util.stream.Collectors;
+
 class Solution {
-
   public int[] solution(String[] genres, int[] plays) {
-      int len = genres.length;
-      List<Integer> answerList = new ArrayList<>();
-      Map<String,Integer> sumMap = new HashMap<>();
-      Map<String, List<Music>> musicMap = new HashMap<>();
+    List<Integer> answerList = new ArrayList<>();
+    int len = genres.length;
+    Map<String, PriorityQueue<int[]>> map = new HashMap<>();
 
-      for(int i = 0; i < len; i++){
-        String key = genres[i];
-        Integer value = plays[i];
-        if(!musicMap.containsKey(key)) musicMap.put(key, new ArrayList<>());
-        sumMap.put(key,sumMap.getOrDefault(key, 0) + value);
-        List<Music> musicList = musicMap.put(key, musicMap.get(key));
-        musicList.add(new Music(key,i, value));
-        musicMap.put(key,musicList);
+    for (int i = 0; i < len; i++) {
+      String key = genres[i];
+      int idx = i;
+      int value = plays[i];
+      if (map.containsKey(genres[i])) {
+        map.get(key).add(new int[]{idx, value});
+      } else {
+        PriorityQueue<int[]> q = new PriorityQueue<>(
+            (arr1, arr2) -> Integer.compare(arr2[1], arr1[1]));
+        q.add(new int[]{idx,value});
+        map.put(key, q);
       }
-      // sort by sumMap DESC
-    List<String> sortedKey = sumMap.entrySet().stream().sorted((k1,k2)->{
-      return Integer.compare(k2.getValue(),k1.getValue());
-    }).map(Entry::getKey).collect(Collectors.toList());
-
-      for(String k : sortedKey){
-        List<Music> sortedMusic = musicMap.get(k).stream().sorted((m1,m2)->{
-          return Integer.compare(m2.value,m1.value);
-        }).collect(Collectors.toList());
-        answerList.add(sortedMusic.get(0).idx);
-        if(sortedMusic.size()>1){
-        answerList.add(sortedMusic.get(1).idx);  
-        }
-        
-      }
-
-      return answerList.stream().mapToInt(i->i).toArray();
-  }
-
-
-  class Music{
-
-    private String genre;
-    private int idx;
-    private int value;
-
-    public Music(String genre, int idx, int value){
-      this.genre= genre;
-      this.idx = idx;
-      this.value = value;
     }
 
+    Set<Entry<String, PriorityQueue<int[]>>> entrySet = map.entrySet();
+
+    List<Entry<String, PriorityQueue<int[]>>> sortedList = entrySet.stream().sorted((v1, v2) -> {
+      int sum1 = v1.getValue().stream().mapToInt(arr -> arr[1]).sum();
+      int sum2 = v2.getValue().stream().mapToInt(arr -> arr[1]).sum();
+      return Integer.compare(sum2, sum1);
+    }).collect(Collectors.toList());
+
+    for (Entry<String, PriorityQueue<int[]>> entry : sortedList) {
+      int idx = 0;
+        while (!entry.getValue().isEmpty() && idx < 2) {
+          answerList.add(entry.getValue().poll()[0]);
+          idx++;
+        }
+      }
+
+
+
+    return answerList.stream().mapToInt(i -> i).toArray();
 
   }
 }
