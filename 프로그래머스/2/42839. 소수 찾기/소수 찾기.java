39476c71203cd89e162be35d17da004b29a4001f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-05 09:07:19 +0900

    [level 2] Title: 소수 찾기, Time: 16.49 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,45 @@
 import java.util.*;
-class Solution {
-    
-    private Set<Integer> set;
-    private boolean[] used;
-    
-    public int solution(String numbers) {
-        set = new HashSet<>();
-        String[] arr = numbers.split("");
-        used = new boolean[arr.length];
-        dfs("",numbers);
-        return set.size();
-        
-        
-        
-    }
-    public void dfs(String pat,String numbers){
-        
-        if(!pat.equals("") && isPrime(Integer.parseInt(pat))) set.add(Integer.parseInt(pat));
-        
-        for(int i = 0; i < numbers.length(); i++){
-            if(pat.length() < numbers.length() && !used[i]){
-            used[i] = true;
-            dfs(pat + numbers.substring(i,i+1),numbers);
-            used[i] = false;
-            }
-        }
-        
-        
-    }
-public boolean isPrime(int n) {
-    if (n <= 1) return false; 
-    if (n == 2) return true;  
-    if (n % 2 == 0) return false; 
 
-    int sqrt = (int) Math.sqrt(n);
-    for (int i = 3; i <= sqrt; i += 2) {
-        if (n % i == 0) {
-            return false; 
-        }
+class Solution {
+    private Set<Integer> set;
+ public static boolean isPrime(int num) {
+    if (num == 1) {
+      return false;
     }
-    return true; 
-}
+    if (num == 2) {
+      return true;
+    }
+    if (num % 2 == 0) {
+      return false;
+    }
+
+    for (int i = 3; i <= Math.sqrt(num); i += 2) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public int solution(String numbers) {
+    set = new HashSet<>();
+    int len = numbers.length();
+    boolean[] visited = new boolean[len];
+    dfs(visited, numbers, "");
+    int answer = (int) set.stream().filter(Solution::isPrime).count();
+    return answer;
+  }
+
+  public void dfs(boolean[] visited, String elements, String curr) {
+    for (int i = 0; i < elements.length(); i++) {
+      if (!visited[i]) {
+        visited[i] = true;
+        set.add(Integer.parseInt(curr + elements.charAt(i)));
+        dfs(visited, elements, curr + elements.charAt(i));
+        visited[i] = false;
+      }
+    }
+  }
+
+    
 }
