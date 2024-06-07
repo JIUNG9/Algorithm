469cfeb4ca285b9c32ddb54e87f5e81e429f0dec
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-07 16:01:43 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,24 +1,7 @@
--- 코드를 입력하세요
-# with get_rented_at_10_16 as (
-#     select history_id, "대여중" as availability 
-#     from car_rental_company_rental_history
-#     where start_date <= "2022-10-16" and end_date >= "2022-10-16"
-
-# )
-
-# select distinct crc.car_id,
-# case 
-# when gr.availability like "대여중" then "대여중"
-# else "대여 가능"
-# end as availability
-# from car_rental_company_rental_history as crc left join get_rented_at_10_16 as gr on gr.history_id = crc.history_id
-# order by car_id desc
-
-select car_id, 
-    case when max(availability) = '1' then '대여중' 
-    else '대여 가능' end as availability 
-from CAR_RENTAL_COMPANY_RENTAL_HISTORY as A 
-    left join (select history_id, '1' as availability from CAR_RENTAL_COMPANY_RENTAL_HISTORY where start_date <= '2022-10-16' and end_date >= '2022-10-16') as B 
-    on A.history_id = B.history_id 
-group by car_id 
-order by car_id desc
+select car_id,
+case 
+when sum(case when "2022-10-16" between start_date and end_date then 1 else 0 end) = 1 then '대여중'
+else '대여 가능' end as availability
+from car_rental_company_rental_history
+group by car_id
+order by 1 desc
