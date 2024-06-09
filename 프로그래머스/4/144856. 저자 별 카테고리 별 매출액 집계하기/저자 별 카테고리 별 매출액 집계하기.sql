7cbcec30940c1e8f16e029344cebcc0319b7709a
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-10 05:42:04 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,11 +1,18 @@
--- 코드를 입력하세요
-with get_author_name_with_book as(
-    select b.*, a.author_name
-    from book as b inner join author as a on b.author_id = a.author_id
-)
-
-select author_id, author_name, category,  sum(sales*price) as total_sales
-from book_sales as bs inner join get_author_name_with_book as ab on bs.book_id = ab.book_id
+select a.author_id, author_name, category, sum(sales* price) as total_sales
+from book as b inner join author as a on b.author_id = a.author_id inner join book_sales as bs on bs.book_id = b.book_id
 where sales_date like "2022-01%"
-group by ab.author_id, b.category
-order by ab.author_id asc, b.category desc
+group by author_id, category
+order by 1 asc, 3 desc
+
+
+-- 코드를 입력하세요
+# with get_author_name_with_book as(
+#     select b.*, a.author_name
+#     from book as b inner join author as a on b.author_id = a.author_id
+# )
+
+# select author_id, author_name, category,  sum(sales*price) as total_sales
+# from book_sales as bs inner join get_author_name_with_book as ab on bs.book_id = ab.book_id
+# where sales_date like "2022-01%"
+# group by ab.author_id, b.category
+# order by ab.author_id asc, b.category desc
