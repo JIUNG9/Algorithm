1c3c0c043c9deeee65553b855f2f30815c0bd649
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-18 06:48:27 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,23 +1,24 @@
-with get_score as 
-(
-    select emp_no, avg(score) as score
+with get_grade as (
+    select
+    case
+    when avg(score) >= 96 then 'S'
+    when avg(score) >= 90 then 'A'
+    when avg(score) >= 80 then 'B'
+    else 'C' end as grade,
+    emp_no
     from hr_grade
     group by emp_no
+
 )
 
 
-select gs.emp_no, emp_name, 
-case 
-when score >= 96 then 'S'
-when score >= 90 then 'A'
-when score >= 80 then 'B'
-else 'C'
-end as grade , 
+
+select emp.emp_no, emp.emp_name, grade, 
 case
-when score >= 96 then round(sal * 0.2,0)
-when score >= 90 then round(sal * 0.15,0)
-when score >= 80 then round(sal * 0.1,0)
+when grade like 'S' then round(sal*0.2,0)
+when grade like 'A' then round(sal*0.15,0)
+when grade like 'B' then round(sal*0.1,0)
 else 0
-end as bonus 
-from get_score as gs inner join hr_employees as he on gs.emp_no = he.emp_no
-order by gs.emp_no
+end as bonus
+from hr_employees as emp inner join get_grade as gg on gg.emp_no = emp.emp_no
+order by 1 asc
