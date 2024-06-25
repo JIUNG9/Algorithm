c091779767f9484e0d0d73bea1181912b94f50ca
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-25 14:39:05 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.07 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,36 +1,48 @@
+import java.util.*;
 class Solution {
     public String solution(String s) {
-        //s의 길이가 200 문자열 -> 띄어쓰기로만해도 200개 -> n^2을 해도 괜찮다.
-        //숫자는 단어의 첫 문자로만 나옵니다. -> 매번 숫자인지 아닌지 확인, 숫자가 아니라면 upperCase
-        //구현 방법: 공백 기준으로 문자열을 나눈 후 첫 문자가 숫자가 아니라 영어라면 upper case로 수정
-        //이후 하나의 문자열로 합치기
+        int len = s.length();
+        StringBuilder sb = new StringBuilder();
+        boolean isFirstLetterAfterWhiteSpace = true;
         
-        
-        String answer = "";
-        s = s.toLowerCase();
-        
-        for(int i = 1; i < s.length(); i++){
-            boolean flag = false;
-            while(s.charAt(i) ==' ' && i < s.length()-1){
-                    answer = answer.concat(" ");                        
-                    i++;
-                flag = true;
-                
+        for(int i = 0; i < len; i++){
+            while(i < len && s.charAt(i)==' '){
+                sb.append(s.charAt(i++));
+                isFirstLetterAfterWhiteSpace = true;
             }
-            
-            if(flag){
-                answer = answer.concat(String.valueOf(Character.toUpperCase(s.charAt(i))));
+            if(i < len){
+                if(isFirstLetterAfterWhiteSpace && isLetter(s.charAt(i))){
+                    isFirstLetterAfterWhiteSpace = false;
+                    sb.append(changeToUpper(s.charAt(i)));
+                }
+                else if(!isLetter(s.charAt(i))){
+                    isFirstLetterAfterWhiteSpace = false;
+                    sb.append(s.charAt(i));
+                }
+                else if(isLetter(s.charAt(i))&& !isFirstLetterAfterWhiteSpace){
+                    sb.append(changeToLowerCase(s.charAt(i)));
+                    
+                }
             }
-            else{
-                answer = answer.concat(String.valueOf(s.charAt(i)));
-                
-            }
-            
             
         }
-        
-        
-        return String.valueOf(s.charAt(0)).toUpperCase() + answer;
-        
+
+       
+            
+            return sb.toString();
+        }
+
+    public boolean isLetter(char c){
+        return Character.isLetter(c);
     }
-}
+    
+    public char changeToUpper(char c){
+        return Character.toUpperCase(c);
+    }
+    
+    public char changeToLowerCase(char c){
+        return Character.toLowerCase(c);
+    }
+    
+    }
+
