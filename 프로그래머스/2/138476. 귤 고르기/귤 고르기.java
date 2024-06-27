223884d0211d0a786c023fa24c462b5798377bab
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-27 15:12:39 +0900

    [level 2] Title: 귤 고르기, Time: 77.25 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,23 +1,29 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
+    private int counter = 0;
+    private Map<Integer,Integer> map;
     public int solution(int k, int[] t) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i =0; i < t.length; i++){
-            map.put(t[i], map.getOrDefault(t[i],0)+1);
+        map = new HashMap<>();
+        
+        for(int i = 0; i < t.length; i++){
+            map.put(t[i],map.getOrDefault(t[i],0)+1);
         }
         
-        List<Integer> list = new ArrayList<>(map.values());
+        List<Integer> valuesDesc = map.values().stream().sorted((i1,i2)->{
+            return Integer.compare(i2,i1);
+        }).collect(Collectors.toList());
         
-        Collections.sort(list, Collections.reverseOrder());
         
-        int counter =0;
-        for(Integer i: list){
-            // System.out.println("i: "+ i);
+        for(Integer i: valuesDesc){
             if(k <= 0) break;
             k-=i;
             counter++;
+        }    
             
-        }
         return counter;
+            
+    
     }
 }
