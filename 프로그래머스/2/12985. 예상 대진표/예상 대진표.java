8d35427b6e23161edf656566363d0549939b4e2d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-06-28 09:36:20 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,28 +1,35 @@
-import java.util.*;
 class Solution
 {
+    private int small;
+    private int big;
+    private int counter = 0;
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        int temp = b;
         
+        assignTheSmallAndBig(a,b);
+
+        while(!(small%2==1 && big %2 ==0 && big - small == 1)){
+            counter++;
+            small = updateToNext(small);
+            big = updateToNext(big);
+        }
+            return counter+1;
+        
+    }
+    
+    public int updateToNext(int i){
+        return i%2==1 ? i/2+1 : i/2;
+    }
+    public void assignTheSmallAndBig(int a, int b){
         if(a > b){
-            b = a;
-            a = temp;
+            big = a;
+            small = b;
+        }
+        else{
+            small = a;
+            big = b;
         }
         
-
-        
-        while(a!=b){
-            answer++;
-            b= getNextParticipationNumber(b);
-            a= getNextParticipationNumber(a);    
-        }
-        
-
-        return answer;
     }
-    public int getNextParticipationNumber(int number){
-        return number%2 ==1 ? number/2+1 : number/2;
-    }
+    
 }
