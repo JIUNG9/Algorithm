a5b6314396828607ab3c9ecd22b2303097734c6e
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-02 06:27:57 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,24 +1,27 @@
-with get_grade as (
-    select
-    case
+-- 코드를 작성해주세요
+
+
+with get_score_with_bonus as(
+
+    select emp_no, 
+    case 
     when avg(score) >= 96 then 'S'
     when avg(score) >= 90 then 'A'
     when avg(score) >= 80 then 'B'
-    else 'C' end as grade,
-    emp_no
+    else 'C'
+    end as grade
     from hr_grade
     group by emp_no
+    
 
 )
 
-
-
-select emp.emp_no, emp.emp_name, grade, 
-case
-when grade like 'S' then round(sal*0.2,0)
-when grade like 'A' then round(sal*0.15,0)
-when grade like 'B' then round(sal*0.1,0)
+select emp.emp_no, emp_name, grade, 
+case 
+when grade like 'S' then sal * 0.2
+when grade like 'A' then sal * 0.15
+when grade like 'B' then sal * 0.1
 else 0
 end as bonus
-from hr_employees as emp inner join get_grade as gg on gg.emp_no = emp.emp_no
+from get_score_with_bonus as gcwb inner join hr_employees as emp on emp.emp_no = gcwb.emp_no
 order by 1 asc
