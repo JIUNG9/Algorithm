6c93c55380b9c30be1d9cfda8066ad3c93a75a21
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-03 09:26:47 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+select board_id, writer_id, title, price, 
+case 
+when status like "DONE" then "거래완료"
+when status like "SALE" then "판매중"
+when status like "RESERVED" then "예약중"
+end as status
+from used_goods_board
+where created_date like "2022-10-05"
+order by 1 desc
