c711796dcd12ddb5b495df687f49f60472956ac1
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-03 17:03:01 +0900

    [level 2] Title: 기능개발, Time: 1.86 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,51 +1,54 @@
 import java.util.*;
+
 class Solution {
+    List<Integer> answerList;
+    List<Stack<Integer>> stkList;
     public int[] solution(int[] p, int[] s) {
-        Queue<Integer> needDays = new LinkedList<>();
-        List<Integer> list = new ArrayList<>();
-        int counter = 0;
-        int len = p.length;
-        //init stk
-        for(int i = 0; i < len; i++){
-            needDays.add(getTakeDays(p[i],s[i]));
+        
+        stkList = new ArrayList<>();
+        answerList = new ArrayList<>();
+        
+        init(p,s);
+        
+        while(!stkList.isEmpty()){
+            proceedTheDay();
+            int counter = todayDeployCouter();
+            if(counter !=0) answerList.add(counter);
         }
         
+        return answerList.stream().mapToInt(i->i).toArray();
         
         
-        while(!needDays.isEmpty()){
-            counter++;
-            int val = needDays.poll();
-            while(!needDays.isEmpty() && needDays.peek() <= val){
-                needDays.poll();
-                counter++;
+    }
+    public void init(int[] p, int[] s){         
+        for(int i = 0; i < p.length; i++){
+            Stack<Integer> stk = new Stack<>();    
+            int currentProgress = p[i];
+            int currentSpeed = s[i];
+            while(currentProgress < 100){
+                stk.push(currentProgress);
+                currentProgress += currentSpeed;
             }
-            list.add(counter);
-            counter  = 0;
-        }
-        
-        
-        
-     return  listToArr(list);
-    }
-    
-    public int[] listToArr(List<Integer> list){
-        int size =list.size();
-        int[] arr = new int[size];
-        
-        for(int i = 0; i < size; i++){
-            arr[i] = list.get(i);
-        }
-        return arr;
-    }
-    
-    public int getTakeDays(int progress, int speed){
-        int numb = 100 - progress;
-        if(numb % speed != 0){
-            return numb / speed + 1;
-        }
-        else{
-            return numb / speed;
+            stkList.add(stk);
         }
     }
+    public void proceedTheDay(){
+        for(Stack<Integer> stk : stkList){
+            if(!stk.isEmpty()) stk.pop();
+            
+        }
+    }
+    public int todayDeployCouter(){
+        int counter = 0;
+        
+        while(stkList.size() > 0 && stkList.get(0).isEmpty()){
+            counter++;
+            stkList.remove(0);
+        }
+        
+        return counter;
+
+        
+        }
     
 }
