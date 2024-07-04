ffc875090f3ae8f3dbf0fdcbe24445c7eb710357
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-04 13:59:22 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,43 +1,28 @@
 class Solution {
-    private int answer = 0;
     private boolean[] visited;
+    private int counter = 0;
     public int solution(int n, int[][] c) {
-        int len = c.length;
-        visited = new boolean[len];
-        
-        for(int i = 0; i < len; i++){
-            for(int j = 0; j < len; j++){
-                if(i!=j && !visited[i]){
-                    visited[i] = true;
-                    dfs(i,c);
-                    answer++;
-                }
-            }
-        }
-        
-        return answer == 0 ? len : answer;
-        /*
-        for(i ; i < len; i++)
-        if(방문하지 않았고 연결되어있다면)
-        연속적으로 연결된 것을 확인(dfs)
-        연결된 것은 하나의 간선으로 간주하고 answer 값을 추가
-        */
-    }
-    
-    public void dfs(int connectedIdx, int[][] c){
-        /*
-        for(i;i < len; i++)
-        if(currentIdx!= i && 방문하지 않았다면)
-        방문처리
-        dfs(connectedIdx, i)
-        */
+        visited = new boolean[n];
         for(int i = 0; i < c.length; i++){
-            if(connectedIdx!=i && c[connectedIdx][i] == 1 && !visited[i]){
+            if(!visited[i]){
                 visited[i] = true;
                 dfs(i,c);
+                counter++;
             }
         }
+        return counter;
+        
     }
+    public void dfs(int currentNode, int[][] c){
         
+        for(int i = 0; i < c.length; i++){
+                if(currentNode != i){
+                    if( c[i][currentNode] == 1 &&  !visited[i]){
+                        visited[i] = true;
+                        dfs(i,c);
+                    }
+            }
+        }
         
+    }
 }
