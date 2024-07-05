ccb33e962e3cf7c4fdcaad6bac9498ec3a40cefd
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-05 10:46:46 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 2.86 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,70 +1,48 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-    public int solution(int[] arrayA, int[] arrayB) {
-        int answer = 0;
-        Arrays.sort(arrayA);
-        Arrays.sort(arrayB);
-        List<Integer> factorListA = getFactorByMinValue(arrayA);
-        List<Integer> factorListB = getFactorByMinValue(arrayB);
+    Set<Integer> set;
+    
+        //A와 B의 공약수를 담은 것을 구하여, 해당 공약 수중 한 쪽은 모두 나누어 떨어지지만 한쪽은 모두 나누어 떨어지지 않는다면 해당 수를 반환한다. 그것이 아니라면 0을 반환한다.
+    //공약수는 공통인 약수 가장 작은 값의 약수를 구하여 계산한다.
+    
+    
+    public int solution(int[] a, int[] b) {
+        set = new HashSet<>();
+        int minA = getMinValue(a);
+        int minB = getMinValue(b);
         
-        factorListA.sort((i1,i2)->{
-            return i1.compareTo(i2);
-        });
-        factorListB.sort((i1,i2)->{
-            return i1.compareTo(i2);
-        });
-        //get max value from the A 
-        for(int i = factorListA.size()-1; i >= 0; i--){
-            int input = factorListA.get(i);
-            //모든원소가 나누어 떨어지지 않는 경우
-            if(!isAtLeastElementDiviedByInput(arrayB,input)){
-                answer = Math.max(answer,input);
+        addCommonDivisor(minA);
+        addCommonDivisor(minB);
+        
+        List<Integer> sortedList = set.stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
+        
+        for(Integer i: sortedList){
+           long aCounter = Arrays.stream(a).filter(num -> num%i == 0).count();
+            long bCounter = Arrays.stream(b).filter(num -> num%i == 0).count();
+            if((aCounter ==0 && bCounter == b.length) || (bCounter == 0 && aCounter == a.length)) return i;
+        }
+        return 0;
+    
+    }
+
+    
+    public void addCommonDivisor(int num){
+        for(int i = 1; i <= Math.sqrt(num); i++){
+            if(num % i == 0){
+                set.add(num/i);
+                set.add(i);
             }
         }
-        
-        for(int i = factorListB.size()-1; i >= 0; i--){
-                int input = factorListB.get(i);
-                //모든원소가 나누어 떨어지지 않는 경우
-                if(!isAtLeastElementDiviedByInput(arrayA,input)){
-                    answer = Math.max(answer,input);
-                }
-        }
-        
-        
-        return answer;
     }
-    
-    
-    
-    List<Integer> getFactorByMinValue(int[] sortedArr){
-       int min = sortedArr[0];
-        List<Integer> factorList = new ArrayList<>();
-        List<Integer> commonFactor = new ArrayList<>();
-        for(int i = min; i > 1; i--){
-            if(min % i == 0) factorList.add(i);
+    public int getMinValue(int[] arr){
+        int min = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            if(min > arr[i]) min = arr[i];
         }
-        
-  
-            for(int k = factorList.size()-1 ; k >=0; k--){
-                boolean flag = true;
-                int factor = factorList.get(k);
-                for(int j = 0; j < sortedArr.length; j++){
-                if(sortedArr[j] % factor != 0) 
-                {
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) commonFactor.add(factor);
-        }
-        
-        return commonFactor;
+        return min;
     }
+
     
-    public boolean isAtLeastElementDiviedByInput(int[] arr, int input){
-        for(Integer i : arr){
-            if(i % input == 0) return true;
-        }
-        return false;
-    }
 }
