f55efb8d6b7febda268fbc8633bd7fd65411b806
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-08 14:18:50 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,22 +1,14 @@
-with get_max as (
+with get_type_max_length as (
+
     select fish_type, max(length) as length
     from fish_info
-    group by fish_type
+    group by fish_type 
 
 )
+, get_type_max as (
+select id, fi.fish_type, fi.length
+from get_type_max_length as ml inner join fish_info as fi on ml.fish_type = fi.fish_type and ml.length = fi.length
+    )
 
-select id,fish_name,length
-from fish_info as fi inner join fish_name_info as fni on fi.fish_type = fni.fish_type
-where (fi.fish_type,fi.length) IN (select fish_type, length from get_max)
-order by fi.id
-
-
-
-# SELECT fi.ID, fn.FISH_NAME, fi.LENGTH
-# FROM FISH_INFO AS fi
-# INNER JOIN FISH_NAME_INFO AS fn
-# ON fi.FISH_TYPE = fn.FISH_TYPE
-# WHERE (fi.FISH_TYPE, fi.LENGTH) IN (
-# SELECT FISH_TYPE, MAX(LENGTH)
-# FROM FISH_INFO
-# GROUP BY FISH_TYPE)
+select id, fish_name, length
+from fish_name_info as fni inner join get_type_max as tm on tm.fish_type = fni.fish_type
