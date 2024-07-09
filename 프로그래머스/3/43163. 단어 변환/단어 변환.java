a7fe86aae951096e579d15d9bd50e41606354a96
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-09 15:18:48 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,37 +1,37 @@
+
 class Solution {
+    private int answer = Integer.MAX_VALUE;
     private boolean[] visited;
-    private int answer = Integer.MAX_VALUE; 
-    
-    public int solution(String b, String t, String[] w) {
-        int len = w.length;
-        visited = new boolean[len];     
-        dfs(b,t,0,w);
+    public int solution(String begin, String target, String[] words) {
+        visited = new boolean[words.length];
+        dfs(begin, target,words, 0);
         return answer == Integer.MAX_VALUE ? 0 : answer;
         
+        
     }
-    public void dfs(String current, String target, int counter, String[] w){
-        
-        
-         if(current.equals(target)){
+    public void dfs(String current, String target, String[] words, int counter){
+        for(int i = 0; i < words.length; i++){
+            if(current.equals(target)){
                 answer = Math.min(answer, counter);
-        }
-        for(int i =0; i < w.length; i++){
-
-            if(!visited[i] && isDifferentOnlyOneWord(current,w[i])){
-                visited[i] = true;
-                dfs(w[i],target,counter+1,w);
-                visited[i] = false;
             }
+            else{
+                if(!visited[i] && isOneCharacterDifferent(current,words[i])){
+                    visited[i] = true;
+                    dfs(words[i],target, words,counter+1);
+                    visited[i] = false;
+                }
+            }
+            
         }
-        
         
     }
-    public boolean isDifferentOnlyOneWord(String s1, String s2){
+    public boolean isOneCharacterDifferent(String a, String b){
         int differentCounter = 0;
-        for(int i = 0; i < s1.length(); i++){
-            if(s1.charAt(i) != s2.charAt(i)) differentCounter++;
-            if(differentCounter >= 2) return false; 
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i) != b.charAt(i)) differentCounter++;
+            if(differentCounter > 1) return false;
         }
         return true;
     }
+    
 }
