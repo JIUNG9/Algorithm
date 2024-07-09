eb0b14ce4aaf837ea94614e4b9f7c214395443f1
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-09 12:28:25 +0900

    [level 3] Title: 여행경로, Time: 12.54 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,41 +1,38 @@
 import java.util.*;
+import java.util.stream.*;
+
 class Solution {
-    List<String> answerList;
-    boolean[] visited;
+    private List<String> answerList;
+    private boolean[] visited;
+    
     public String[] solution(String[][] t) {
         int len = t.length;
-        String dep = "ICN";
         answerList = new ArrayList<>();
         visited = new boolean[len];
-        dfs(visited, 0, len, dep, "", t);
+    
+        dfs("ICN","ICN",0,t);
         
-        answerList.sort((s1,s2)->{
-            return s1.compareTo(s2);
-        });
+        return answerList.stream().sorted().collect(Collectors.toList()).get(0).split(",");
         
-        String answer = "ICN "+ answerList.get(0);
-        return answer.split(" ");
-            
+        
         
         
     }
     
-    
-    public void dfs(boolean[] visited, int depth, int len, String dep, String words, String[][] t){
-        //depth가 len과 일치할 때만 Heap 영역에 있는 리스트에 추가한다.
-        if(len == depth){
-          answerList.add(words.substring(1));
-        } 
-        
-     
-        for(int i =0; i < len; i++){
-            if(!visited[i] && dep.equals(t[i][0])){
-                visited[i] = true;
-                dfs(visited, depth+1, len, t[i][1], words + " " + t[i][1], t);
-                visited[i] =false;
-            }
-        
+    public void dfs(String route,String recent, int depth, String[][] t){
+        if(depth == t.length){
+            answerList.add(route);
         }
-        
+        else{
+            for(int i = 0; i < t.length; i++){
+                if(!visited[i] && recent.equals(t[i][0])){
+                    visited[i] = true;
+                    dfs(route+","+t[i][1],t[i][1],depth+1,t);
+                    visited[i] = false;
+                }
+            }
+        }
+    
+    
     }
 }
