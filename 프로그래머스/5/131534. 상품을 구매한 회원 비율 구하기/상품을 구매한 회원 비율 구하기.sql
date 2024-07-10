8a28b38a79dedae05dbd223f167c126b08efeaca
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-10 15:01:55 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,22 +1,23 @@
 -- 코드를 입력하세요
-with get_member_joined_counter_at_2021 as(
-    select count(*) as num
+with sign_up_at_2021 as (
+
+    select *
     from user_info
-    where joined like "2021%"
+    where year(joined) = 2021
 )
 
-, member_who_joined_at_2021 as(
-
-    select user_id
+, count_2021 as (
+    select count(*) as cnt
     from user_info
-    where joined like "2021%"
-
+    where year(joined) = 2021
 )
 
+select year(sales_date) as year, month(sales_date) as month, count(distinct su.user_id)  as purchased_users, round((count(distinct su.user_id)/cnt) ,1) as puchased_ratio
+from sign_up_at_2021 as su inner join online_sale as os on os.user_id = su.user_id, count_2021
+group by 1, 2
+order by 1,2 
 
-select YEAR(sales_date) as year, Month(sales_date) as month, count(distinct os.user_id) as purchased_users, round(count(distinct os.user_id)/num,1) as puchased_ratio
-from member_who_joined_at_2021 as mw inner join online_sale as os on os.user_id = mw.user_id, get_member_joined_counter_at_2021
-group by year,month
-
+# select count(distinct su.user_id)
+#  from sign_up_at_2021 as su inner join online_sale as os on os.user_id = su.user_id, count_2021
 
 
