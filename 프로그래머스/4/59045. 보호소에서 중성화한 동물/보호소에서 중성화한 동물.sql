3f321ebd9252b61584ed846268f0135d1f9664cd
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-11 09:15:05 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,10 +1,5 @@
 -- 코드를 입력하세요
-with get_not_neuter_when_in as (
-    select animal_id
-    from animal_ins
-    where sex_upon_intake like "Intact%"
-)
-
 select ao.animal_id, ao.animal_type, ao.name
-from animal_outs as ao inner join get_not_neuter_when_in as ai on ao.animal_id = ai.animal_id
-where ao.sex_upon_outcome like "Neutered%" or ao.sex_upon_outcome like "Spayed%"
+from animal_ins as ai inner join animal_outs as ao on ai.animal_id = ao.animal_id
+where ai.sex_upon_intake like "Intact%" and (ao.sex_upon_outcome like "Neutered%" or ao.sex_upon_outcome like "Spayed%")
+order by 1 asc
