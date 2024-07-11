509de6da7d707415d1183863034416105f7531ab
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-11 11:07:47 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,9 +1,10 @@
--- 코드를 입력하세요
 
+-- domain: the date time at the animals_in is the date about protection data from when, otherwise 
+-- the date time in animals_out is the date about adapt the animal
+-- logical : 빠르다 -> 더 이전이다(past)
+-- Impl: join by the animal_id and filter the data which ai.datetime < ao.datetime, order by ai.datetime asc
 
-
-
-SELECT AI.ANIMAL_ID, AI.NAME
-FROM ANIMAL_INS AS AI INNER JOIN ANIMAL_OUTS AS AO ON AI.ANIMAL_ID = AO.ANIMAL_ID
-WHERE AI.DATETIME > AO.DATETIME
-ORDER By AI.DATETIME ASC
+select ai.animal_id, ai.name
+from animal_ins as ai inner join animal_outs as ao on ai.animal_id = ao.animal_id
+where ai.datetime > ao.datetime
+order by ai.datetime asc
