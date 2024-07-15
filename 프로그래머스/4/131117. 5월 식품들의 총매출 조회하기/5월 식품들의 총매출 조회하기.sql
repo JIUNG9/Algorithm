e232849be31992dca5689816799040b95a69577b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-15 11:08:30 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,6 +1,13 @@
 -- 코드를 입력하세요
-select fo.product_id, product_name,sum(amount) * price as total_sales
+-- Domain: There are multiple rows by same product_id in  food_order, food_order has the food_product foreign key which name is the product_id
+-- Impl: 
+-- 1.Use the where clause for getting the only data which is "2022-05"
+-- 2. For getting total_sales group by the product_id by food_order.product_id and multiply by the product price
+
+
+
+select fo.product_id, product_name, sum(amount) * price as total_sales
 from food_order as fo inner join food_product as fp on fo.product_id = fp.product_id
-where fo.produce_date like "2022-05%"
+where produce_date like "2022-05%"
 group by fo.product_id
-order by 3 desc, 1
+order by total_sales desc, 1 asc
