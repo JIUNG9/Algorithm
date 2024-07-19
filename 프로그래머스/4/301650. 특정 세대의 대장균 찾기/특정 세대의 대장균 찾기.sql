d162264d6ca16054c531717fd17bd636f832465a
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-19 10:42:57 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,30 +1,18 @@
 -- 코드를 작성해주세요
-# with recursive tree as(
+with recursive cte as(
+    select id, 1 as gen
+    from ecoli_data
+    where parent_id is null
 
-#     select id , 0 as gen
-#     from ecoli_data
-#     where parent_id is null
+    union all
     
-#     union all
-    
-#     select b.id, gen+1 
-#     from ecoli_data as a right join ecoli_data as b on a.id = b.parent_id
-#     where gen < 3 AND a.id is null
-# )
+    select child.id, (gen+1) as gen
+    from cte inner join ecoli_data as child on cte.id = child.parent_id
+    where gen < 4
 
-# select id
-# from tree
-# order by 1 asc
-
-WITH RECURSIVE WR AS (
-    SELECT ID , PARENT_ID , 1 AS GENERATION 
-    FROM ECOLI_DATA 
-    WHERE PARENT_ID IS NULL
-    
-    UNION ALL
-    
-    SELECT A.ID , A.PARENT_ID , W.GENERATION +1
-    FROM ECOLI_DATA  A INNER JOIN WR W ON A.PARENT_ID = W.ID
-    where generation < 4
 )
-SELECT ID FROM WR WHERE GENERATION = 3 ORDER BY ID
+
+select id
+from cte
+where gen = 3
+order by id asc
