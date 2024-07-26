a62bd688707ddee8441447b48f3de5c9222f974c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-26 15:18:27 +0900

    [level 2] Title: 모음 사전, Time: 6.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,24 +1,23 @@
 class Solution {
-    private String[] vowel ={"A","E","I","O","U"};
-    private int counter = 0;
+    String[] vowel ={"A","E","I","O","U"};
     private int answer = 0;
-    
+    private int counter = 0;
     public int solution(String word) {
         dfs("",word);
         return answer;
+        
     }
     public void dfs(String current, String target){
-        if(current.equals(target)) {
+        if(current.equals(target)){
             answer = counter;
         }
-        if(current.length() >= 5) return;
+        if(current.length() == 5) return;
+        
         else{
             for(String s: vowel){
                 counter++;
-                dfs(current+s, target);
+                dfs(current+s,target);
             }
         }
-        
-        
     }
 }
