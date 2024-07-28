c315922ba19d2326cf959dcf054fef71776bbf50
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-07-29 06:41:30 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,6 +1,14 @@
--- 코드를 입력하세요
-SELECT FOOD_TYPE, REST_ID, REST_NAME,  FAVORITES
-FROM REST_INFO
-WHERE (FOOD_TYPE, FAVORITES) IN(SELECT FOOD_TYPE, MAX(FAVORITES) FROM REST_INFO GROUP BY FOOD_TYPE)
-GROUP BY FOOD_TYPE
-ORDER BY FOOD_TYPE DESC
+-- 음식 종류 별로 즐겨 찾기 수가 가장 많은 식당을 얻어, 해당 식당의 정보를 출력하고 음식의 종류를 기준으로 내림차순 정렬
+-- dense rank function을 통해 음식 종류 별 가장 좋아요 개수가 많은 레스토랑 아이디와 rank를 통해 rank가 1이고 음식 종류가 같은 튜플을 얻고 음식 종류를 기준으로 내림차순한다.
+
+with get_rank as(
+
+    select rest_id, dense_rank() over(partition by food_type order by favorites desc) as rnk
+    from rest_info
+
+)
+
+select food_type, rest_id, rest_name, favorites
+from rest_info as r
+where rest_id in (select rest_id from get_rank where rnk = 1)
+order by 1 desc
