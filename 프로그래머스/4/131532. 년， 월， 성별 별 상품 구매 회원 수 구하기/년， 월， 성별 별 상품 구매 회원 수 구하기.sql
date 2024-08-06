a66d1a1fbe1af9d5ae35df653af887f25cf17cf3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-08-07 06:49:06 +0900

    [level 4] Title: 년, 월, 성별 별 상품 구매 회원 수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
+++ 프로그래머스/4/131532. 년， 월， 성별 별 상품 구매 회원 수 구하기/년， 월， 성별 별 상품 구매 회원 수 구하기.sql
@@ -1,5 +1,20 @@
-select  YEAR(sales_date) as year, MONTH(sales_date) as month, gender, count(distinct(os.user_id))as users
+-- 코드를 입력하세요
+-- Domain: Online sale에는 같은 회원의 아이디가 여러개 존재할 수 있다. 다만 동일한 날짜 회원 ID, 상품 ID는 한 개가 존재한다.
+-- Impl : 년, 월, 성별로 상품을 구매한 회원 수를 구할 때, 같은 년와 월에 같은 회원의 ID는 제외되어야한다. 구매한 횟수가 아니기 때문이다. 따라서 online sales에서 회원으로 한 번 집계를 한 이후에 집계를 진행한다.
+
+
+select year(sales_date)as year,month(sales_date) as month, gender, count(distinct os.user_id) as users
 from user_info as ui inner join online_sale as os on ui.user_id = os.user_id
 where gender is not null
 group by 1,2,3
 order by 1,2,3
+
+
+# SELECT YEAR(SALES_DATE) AS YEAR, MONTH(SALES_DATE) AS MONTH, GENDER, COUNT(DISTINCT O.USER_ID) AS USERS
+# FROM USER_INFO I
+# JOIN ONLINE_SALE O
+# ON I.USER_ID = O.USER_ID
+# WHERE GENDER IS NOT NULL
+# GROUP BY 1,2,3
+# ORDER BY 1,2,3;
+
