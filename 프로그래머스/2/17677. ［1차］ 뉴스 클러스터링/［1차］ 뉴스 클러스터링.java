9416f44712e1747e4de2d72df6dfd0c52d812d80
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-08-06 23:23:04 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 11.33 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,101 +1,72 @@
 import java.util.*;
+import java.util.regex.*;
+
 
 class Solution {
-    public int solution(String s1, String s2) {
-        s1 = toUpper(s1);
-        s2 = toUpper(s2);
-        List<String> list1 = getStringList(s1);
-        List<String> list2 = getStringList(s2);
+    private Pattern p;
+    private int intersectionCount = 0;
+    private int unionCount = 0;
+    public int solution(String str1, String str2) { 
+//소문자, 대문자 상관없이 연속된 영어의 2개의 조합만을 검사하여 두 개의 합집합의 크기와 교집합의 크기를 구하여 교집합의 개수 / 합집합의 개수 * 65536 를 구한다 만약 교집합의 개수가 0이라면 1 * 65536을 구한다.
+    // Str을 각각 모두 Upper case로 구한 뒤 Pattern을 [A-Z]{2}로 컴파일하여 모든 문자열의 순서쌍 케이스를 확인한다. 각각의 문자열의 Map을 생성하여, 각 맵을 서로가 순회하면서 만약 키 값이 일치한다면 큰 값을 가지는 것이 합집합의 원소가 되고, 작은 것이 교집합의 개수가 된다.
+        str1 = str1.toUpperCase();
+        str2 = str2.toUpperCase();
+        Map<String,Integer> map1 = new HashMap<>();
+        Map<String,Integer> map2 = new HashMap<>();
         
-        if(isBothSetEmpty(list1,list2)){
-            return 65536;
-        }   
-     
-        int theNumberOfCommon = getCommonNumber(list1,list2);
-        int theNumberOfUnion = getTheNumberOfUnion(list1.size(),list2.size(),theNumberOfCommon);
-        
-        
-        return multiplyWith65536AndRoundDown(theNumberOfCommon,theNumberOfUnion);
+        p = Pattern.compile("[A-Z]{2}");
             
-            
+        initMapByInputString(str1,map1,p);
+        initMapByInputString(str2,map2,p);
         
-        }
-    public List<String> getStringList(String s){
-        
-        int len = s.length();
-        List<String> list = new ArrayList<>();
-        
-        for(int i = 0; i < len; i++){
-            if(isAlphabet(s.charAt(i))){
-                if(i + 1 < len && isAlphabet(s.charAt(i+1))){
-                        String str = String.valueOf(s.charAt(i)).concat(String.valueOf(s.charAt(i+1)));
-                    System.out.println("str:" + str);
-                        list.add(str);
-                    }
-                }
-            }
-            return list;
+    
+        float answer = getJarcard(map1,map2);
+        // System.out.println("answer: " + answer);
+        return (int)(answer * 65536);
 
-        }
-        
-    
-    public String toUpper(String s){
-        return s.toUpperCase();
-    }
-    
-    public boolean isAlphabet(char c){
-        return Character.isLetter(c);
         
     }
-    
-    
-    public int getTheNumberOfUnion(int list1Size, int list2Size, int intersectionSize){
-        return list1Size + list2Size - intersectionSize;  
-        }
-    
-    
-    public int getCommonNumber(List<String> list1, List<String> list2){
-            List<String> list1Copy = List.copyOf(list1);
-            List<String> list2Copy = new ArrayList<>(List.copyOf(list2));
-            int list1Len = list1Copy.size();
-            int list2Len = list2Copy.size();
-            int counter = 0;
+    private float getJarcard(Map<String,Integer> map1 ,Map<String,Integer> map2){
+        Set<String> keySet1 = map1.keySet();
+        Set<String> keySet2 = map2.keySet();
         
-        for(int i = 0; i < list1Len; i++){
-            for(int j = 0; j < list2Len; j++){
-                if(list1Copy.get(i).equals(list2Copy.get(j))){
-                    counter++;
-                    list2Copy.remove(j);
-                    list2Len-=1;
-                    break;
-                }
-             
+        for(String key : keySet1){
+            if(keySet2.contains(key)){
+                int value1 = map1.get(key);
+                int value2 = map2.get(key);
+                
+                int min = (value1 >= value2) ? value2 : value1;
+                int max = (value1 >= value2) ? value1 : value2;
+                    
+                intersectionCount+=min;
+                unionCount+=max;  
+            }
+            else{
+                unionCount+=map1.get(key);
             }
         }
-return counter;
+        
+        for(String secodnKey : keySet2){
+            if(!keySet1.contains(secodnKey)) unionCount+=map2.get(secodnKey);
+            
+        }
 
+        
+        return intersectionCount == 0 && unionCount == 0 ? 1 : (float)((float)intersectionCount / (float)unionCount);
+        
+    }
+
+    
+    private void initMapByInputString(String str,Map<String,Integer> map, Pattern p){
+        
+        for(int i = 0; i < str.length()-1 ; i++){
+            String current = str.substring(i,i+2);
+            if(p.matcher(current).matches()) {
+                map.put(current,map.getOrDefault(current,0)+1);
+                }
         }
         
-    
-    public int multiplyWith65536AndRoundDown(int common, int union){
-        // type conversion
-    System.out.println("common:" + common);
-        System.out.println("union:" + union);
-        double result = (double) common / (double) union * (double) 65536; 
-            
-            return (int) result;
-        
-        
-        
-        
-        
-    }    
-    
-    public boolean isBothSetEmpty(List<String> list1, List<String> list2){
-        if(list1.size()==0 && list2.size()==0) return true;
-        return false;
         
         
     }
-    
 }
