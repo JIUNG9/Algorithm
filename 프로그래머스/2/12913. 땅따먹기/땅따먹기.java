e577de0b19ac161fd06715848911c8aba95cc2e4
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-08-12 20:23:57 +0900

    [level 2] Title: 땅따먹기, Time: 16.13 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,36 +1,40 @@
-import java.util.*;
-
 class Solution {
-  int solution(int[][] land) 
-    {
-        int answer = 0;
+    private int answer = -1;
+    int solution(int[][] land) {
+        
+        int row = land.length;
+        int[][] dp= new int[row][4];
 
-        // 각 행의 각 열마다 최대값을 저장해 나간다. 
-        int dp[][] = new int[land.length + 1][4];
-
-        // 각 행의 대한 계산
-        for(int i = 1; i <= land.length; i++)
-        {
-            // 각 열에 대한 계산
-            for(int j = 0; j < 4; j++)
-            {
-                // 현재 열을 제외한 나머지 열에 대한 최댓값을 계산한다
-                for(int k = 0; k < 4; k++)
-                {
-                    // 현재 열은 무시
-                    if(k == j)
-                    {
-                        continue;
-                    }
-
-                    // 현재 열을 제외한 나머지 열에 대한 계산값을 비교하고 최댓값을 기록한다. 
-                    dp[i][j] = Math.max(dp[i][j], land[i - 1][j] + dp[i - 1][k]);
-
-                    answer = Math.max(answer, dp[i][j]);
-                }
+        dp[0][0] = land[0][0];
+        dp[0][1] = land[0][1];
+        dp[0][2] = land[0][2];
+        dp[0][3] = land[0][3];
+        
+        
+        for(int i = 1; i< row; i++){
+            int[] prevRow = dp[i-1];
+            for(int j = 0; j < prevRow.length; j++){
+                int max = getMaxValueWithoutCurrIdx(j,prevRow);
+                dp[i][j] +=(land[i][j] + max);
             }
         }
-
+        
+        for(int k = 0; k < 4; k++){
+            answer = Math.max(answer,dp[row-1][k]);
+        }
+    
         return answer;
+
+    }
+    
+    private int getMaxValueWithoutCurrIdx(int idx, int[] row){
+        int max = -1;
+        for(int i = 0; i < 4; i++){
+            if(idx == i) continue;
+            max = Math.max(max,row[i]);
+        }
+        return max;
+        
+        
     }
 }
