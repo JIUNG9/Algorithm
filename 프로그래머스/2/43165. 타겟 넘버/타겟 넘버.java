3beffb0742face6d6a18ae356e74f599d19821d7
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-08-12 21:35:57 +0900

    [level 2] Title: 타겟 넘버, Time: 5.09 ms, Memory: 70.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,23 +1,17 @@
 class Solution {
-    private boolean[] visited;
     private int answer = 0;
-    public int solution(int[] n, int t) {
-        visited = new boolean[n.length];
-        dfs(n,0,0,t);
+    public int solution(int[] numbers, int target) {
+        
+        dfs(target, 0, numbers,0);
         return answer;
-        
     }
-    
-    public void dfs(int[] n, int current, int currentLen,int t){
-        if(current == t && currentLen == n.length){
-          answer++;
-            return;
-        } 
-        if(currentLen < n.length){
-            dfs(n, current + n[currentLen], currentLen + 1, t);
-            dfs(n,current - n[currentLen], currentLen + 1, t);
+    public void dfs(int target, int sum, int[] numbers, int depth){
+        if(depth == numbers.length){
+            if(target == sum) answer++; 
+                return;
         }
+                dfs(target, sum + numbers[depth], numbers, depth+1);
+                dfs(target, sum - numbers[depth], numbers, depth+1);
         
     }
-
 }
