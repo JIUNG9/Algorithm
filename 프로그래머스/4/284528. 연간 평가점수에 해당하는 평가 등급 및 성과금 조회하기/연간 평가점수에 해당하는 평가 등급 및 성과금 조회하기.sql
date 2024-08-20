ba2044606bab6e13d043cabd31d32b81d775dd5d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-08-20 21:19:33 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,27 +1,24 @@
 -- 코드를 작성해주세요
+-- HR_Grade를 통해 사원 별 등급을 구하고(emp_no와 year로 집계하여, score의 총합의 평균을 구하여 기준 점수에 따르는 평가등급을 정한다.) 해당 등급을 통해 보너스를 계산한다. 보너스는 평가표를 기준으로 해당 직원의 연봉을 기준으로 한다.
 
-
-with get_score_with_bonus as(
-
-    select emp_no, 
+with get_grade as (
+    select emp_no,
     case 
-    when avg(score) >= 96 then 'S'
-    when avg(score) >= 90 then 'A'
-    when avg(score) >= 80 then 'B'
+    when sum(score) / 2 >= 96 then 'S'
+    when sum(score) / 2 >= 90 then 'A'
+    when sum(score) / 2 >= 80 then 'B'
     else 'C'
-    end as grade
+    end as rnk
     from hr_grade
-    group by emp_no
-    
-
+    group by emp_no, year
 )
 
-select emp.emp_no, emp_name, grade, 
-case 
-when grade like 'S' then sal * 0.2
-when grade like 'A' then sal * 0.15
-when grade like 'B' then sal * 0.1
-else 0
+select he.emp_no, emp_name, rnk as grade, 
+case
+when rnk like 'S' then round(sal * 0.2,0)
+when rnk like 'A' then round(sal * 0.15,0)
+when rnk like 'B' then round(sal * 0.1,0)
+when rnk like 'C' then 0
 end as bonus
-from get_score_with_bonus as gcwb inner join hr_employees as emp on emp.emp_no = gcwb.emp_no
+from get_grade as gg inner join hr_employees as he on gg.emp_no = he.emp_no
 order by 1 asc
