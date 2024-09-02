fa83cda1ab237bb99c02347f5a5fb101dd832586
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-02 20:42:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.76 ms, Memory: 56.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,39 @@
 import java.util.*;
-
 class Solution {
-    private int[][] move = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
-    private boolean[][] visited;
+    private int answer = -1;
+    private int[][] move = {{-1,0},{0,-1},{1,0},{0,1}};
     public int solution(int[][] maps) {
-
-        int targetX = maps[0].length-1;
-        int targetY = maps.length-1;
-        int length = maps.length;
+        //가려는 곳이 벽이 아니고, 맵의 크기를 넘어서지 않고, 이미 방문한 적이 없다면 해당 장소를 방문한다. 방문한 곳은 방문처리를 하고 이후에는 방문하지 않는다. 해당 과정을 최단 거리로 [n-1][m-1]를 도달하였을 때 반복한 횟수를 반환하여라. 만약 도달할 수 없다면 -1을 반환하여라
+        
+        int len = maps.length;
         int width = maps[0].length;
+        boolean[][] visited = new boolean[len][width];
+        Queue<int[]> q = new LinkedList<>();
         
-        Queue<int[]> q= new LinkedList<>();
-        q.add(new int[]{0,0,1});
-        
-        visited = new boolean[length][width];
-        
-        
-        
-        
+        visited[0][0] = true;
+        q.add(new int[]{1,0,0});
         
         while(!q.isEmpty()){
-            int[] current  = q.poll();
-            int currentY = current[0];
-            int currentX = current[1];
-            int weight = current[2];
-            if(currentX == targetX && currentY == targetY) return weight;
-            else{
-                for(int[] m : move){
-                    int movedY = currentY + m[0];
-                    int movedX = currentX + m[1];
-                    if(movedY >= 0 && movedX >=0 && movedY < length && movedX < width && !visited[movedY][movedX] && maps[movedY][movedX] == 1){
-                        visited[movedY][movedX] = true;
-                        q.add(new int[]{movedY,movedX,weight+1});
-                    }
+            int[] arr = q.poll();
+            int counter = arr[0];
+            int y = arr[1];
+            int x = arr[2];
+            
+            if(y == len -1 && x == width -1) return counter;
+            
+            for(int[] m : move){
+                int newY = y + m[0];
+                int newX = x + m[1];
+                
+                if(newY < len && newX < width && newY >= 0 && newX >=0 &&!visited[newY][newX] && maps[newY][newX] != 0){
+                    visited[newY][newX] = true;
+                    q.add(new int[]{counter+1,newY,newX});
                 }
             }
+            
         }
-        return -1;
+        
+ 
+        return answer;
     }
-    
 }
