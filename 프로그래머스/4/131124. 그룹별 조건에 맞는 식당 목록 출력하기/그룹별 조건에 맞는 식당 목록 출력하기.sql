21ac44214f2c254d45fba77ade9c8175e0e68662
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-12 07:13:20 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,6 +1,16 @@
 -- 코드를 입력하세요
--- Domain: There are multiple rows by same member_id
--- Impl: Use the dense_rank() over() function, partiion by the counter of member_id and order by desc
+
+# with get_rnk as(
+#     select rr.member_id, dense_rank() over(partition by rr.member_id order by count(rr.member_id) desc ) as rnk 
+#     from member_profile as mp inner join rest_review as rr on mp.member_id = rr.member_id
+# )
+
+
+# select member_name, review_text, DATE_FORMAT(review_date,"%Y-%m-%d") as review_date
+# from rest_review r inner join member_profile m on r.member_id = m.member_id
+# where r.member_id in (select member_id from get_rnk where rnk = 1)
+# order by 3 asc, 2 asc
+
 
 with get_most_reviewer as (
     select rr.member_id, dense_rank() over (order by count(*) desc) as rnk
