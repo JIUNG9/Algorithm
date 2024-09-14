e8259e2611d06a4812b0d5df3806e05bd015160c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-14 11:11:59 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,25 +1,16 @@
 -- 코드를 입력하세요
 
-# with get_rnk as(
-#     select rr.member_id, dense_rank() over(partition by rr.member_id order by count(rr.member_id) desc ) as rnk 
-#     from member_profile as mp inner join rest_review as rr on mp.member_id = rr.member_id
-# )
+-- 리뷰를 가장 많이 작성한 회원은 리뷰 테이블에서 존재하는 멤버의 아이디를 그룹지어 멤버의 아이디가 가장 많이 존재하는 멤버의 아이디를 통해 가장 리뷰를 많이 쓴 사람을 구한다.
+-- 해당 유저 테이블에서 유저의 이름과 리뷰 테이블에서 리뷰의 텍스트와 자료 형식을 가공하여 원하는 형식의 데이트를 얻을 수 있다. 위에서 구한 결과에서 rnk가 1인 사람을 review에서 검색 조건으로 두어 결과 값을 얻는다.  결과 값은 리뷰 작성일 기준 오름차순, 리뷰 텍스트를 기준으로 오름차순한다.
 
-
-# select member_name, review_text, DATE_FORMAT(review_date,"%Y-%m-%d") as review_date
-# from rest_review r inner join member_profile m on r.member_id = m.member_id
-# where r.member_id in (select member_id from get_rnk where rnk = 1)
-# order by 3 asc, 2 asc
-
-
-with get_most_reviewer as (
-    select rr.member_id, dense_rank() over (order by count(*) desc) as rnk
-    from member_profile as mp inner join rest_review as rr on mp.member_id = rr.member_id
-    group by rr.member_id
+with get_rnk as(
+    select member_id, dense_rank() over(order by count(member_id) desc) as rnk
+    from rest_review rr
+    group by member_id
 )
 
 
 select member_name, review_text, date_format(review_date,"%Y-%m-%d") as review_date
-from member_profile as mp inner join rest_review as rr on mp.member_id = rr.member_id 
-where rr.member_id in (select member_id from get_most_reviewer where rnk = 1)
+from rest_review rr inner join member_profile mp on rr.member_id = mp.member_id
+where rr.member_id in (select member_id from get_rnk where rnk = 1)
 order by 3,2
