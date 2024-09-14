73ad7ce61ee633fb424b190e89e06f11b04b2b00
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-14 10:23:11 +0900

    [level 2] Title: 디펜스 게임, Time: 65.93 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,40 +1,57 @@
-import java.util.PriorityQueue;
-
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
- public int solution(int n, int k, int[] enemy) {
-    int len = enemy.length;
-    int idx = 0;
-    int counter =0;
-    PriorityQueue<Integer> q = new PriorityQueue<>();
-
-      while (idx < len && k > 0) {
-        k--;
-        q.add(enemy[idx++]);
-        counter++;
-      }
-      while (idx < len && n > 0) {
-        if (!q.isEmpty() && enemy[idx] > q.peek()) {
-          if (n - q.peek() >= 0) {
-            n-=q.poll();
-            q.add(enemy[idx++]);
-            counter++;
-          }
-          else{
-            return counter;
-          }
-        } else {
-          if(n-enemy[idx] >=0){
-            n-=enemy[idx++];
-            counter++;
-          }
-          else{
-            return counter;
-          }
+    public int solution(int n, int k, int[] enemy) {
+        //최적의 해 -> 가장 적합할 때 무적권을 사용해야한다. 그럼 언제?
+        //병사를 다 써버린 이후에 무적권을 사용하는 것은 최적의 해를 보장하지 않는다.
+        //평균을 구해서 무적권을 사용하는 것도 보장할 수 없다.
+        //즉 병사를 사용하다 더 이상 막아낼 수 없을 때 이전에 처리했던 가장 큰 인원수의 병력을 무적권을 사용하여 처리하는 것이 가장 최적의 해를 보장한다.
+        //구현 방법: PQ를 사용한다.
+        int len = enemy.length;
+        int counter = 0;
+        int i = 0;
+        
+        PriorityQueue<Integer> q = new PriorityQueue<>(Collections.reverseOrder());
+        
+        while(i < len){
+            //더 이상 진행할 수 없을 경우
+            if(k <=0 && n - enemy[i] < 0) break;
+            //막아낼 수 있을 때까지 병사 사용
+            while(i < len && n - enemy[i] >= 0){
+                n-=enemy[i];
+                q.add(enemy[i]);
+                i++;
+                counter++;
+            }
+            //현재는 막을 수 없는 상태 따라서 무적권이 존재한다면 
+            if(i < len && k > 0){
+                //이전에 처리했던 가장 큰 값을 무적권을 사용하여 현재 적을 처리할 수 있게 함
+                if(q.size() > 0){
+                    //edge case 
+                    if(enemy[i] > q.peek()){
+                        i++;
+                        counter++;
+                    }
+                    else{
+                        n+=q.poll();     
+                    }
+                    
+                    k--;
+                }
+                //만약 이전에 처리했던 것이 없다면 무적권을 사용해서 현재 다가오는 병력을 막고 다음 것을 처리할 준비
+                else{
+                    k--;
+                    i++;
+                    counter++;
+                }
+                
+            }
+            
         }
-      }
-    return counter;
-
-  }
-
+            
+            return counter;
+        }
+        
+        
 }
