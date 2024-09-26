2b963d0173a99ef870b9517c68f3ad9f41decf9b
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-26 09:31:45 +0900

    [level 2] Title: 올바른 괄호, Time: 18.56 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,25 +1,21 @@
 import java.util.*;
 class Solution {
-    
-    Stack<Character> stk;
     boolean solution(String s) {
-        stk = new Stack<>();
-        int len = s.length();
+        //바르게 짝 지어졌다. 순서가 필요하다. -> Stack, Queue
+        //왼쪽 괄호가 두 개라면 오른쪽 괄호도 두 개가 나와야한다.
+        //그리고 모든 게 끝났을 때 stk에 남는 개수가 없어야한다.
+        //구현 스택을 사용해서 왼쪽 괄호라면 스택에 넣고 오른쪽 괄호일 때 해당 스택에 왼쪽 괄호가 존재하는지 확인한다 만약 존재 하지 않는다면 false를 리턴한다.
         
-        for(int i = 0; i < len; i++){
-            char currentC = s.charAt(i);
-            if(currentC == '(') stk.push(currentC);
+        Stack<Character> stk = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i)=='(') stk.push('(');
             else{
-                if(!stk.isEmpty()) stk.pop();
-                else return false;
+                if(stk.isEmpty()) return false;
+                stk.pop();
             }
         }
-        if(stk.isEmpty()) return true;
-        return false;
-
-        
-        
-
+        return stk.isEmpty();
         
     }
 }
