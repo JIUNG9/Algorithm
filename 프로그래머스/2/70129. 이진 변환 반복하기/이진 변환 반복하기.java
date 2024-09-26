3480106ecd3d82e765c37e2acdca9a2bdcbd8876
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-27 08:15:54 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.67 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,39 +1,41 @@
 class Solution {
-   public int[] solution(String s) {
-
-    //1. count the number of iteration
-    //2. count the removed '0' numbers
-    //3. loop while s.length is not 1
-    //-> while loop count removed 0 numbers
-    //-> while loop count iteration number
-    //-> while loop change the number to binary which is removed 0
-    int iterationCounter = 0;
-    int removed0Counter = 0;
-    while(s.length()!=1){
-      StringBuilder sb = new StringBuilder();
-      for(int i = 0; i < s.length(); i++){
-        if(s.charAt(i)=='0') removed0Counter++;
-        else{
-          sb.append(s.charAt(i));
+    private int removedCounter = 0;
+    public int[] solution(String s) {
+        
+        //이진 변화 결과가 1이 될때까지,제거할 0의 개수를 구하고 현재 s길이에서 해당 개수를 뺸 수를 다시 이진 수를 만든다. 이때 제거할 0의 개수의 총합을 구하고, 몇 번 반복했는지 횟수를 구하여, 반복횟수를 배열의 첫 번째 원소로, 제거한 개수를 두 번째 원소로 구하여 반환하여라
+        
+        int round = 0;
+        while(!s.equals("1")){
+            System.out.println("s: "+ s);
+            round++;
+            s = getNextBinary(s);
         }
-      }
-      iterationCounter++;
-      s = decimalToBinary(sb.toString().length());
+        
+        return new int[]{round, removedCounter};
     }
-
-    return new int[]{iterationCounter,removed0Counter};
-  }
-
-  public String decimalToBinary(int value){
-    StringBuilder sb = new StringBuilder();
-    while(value!=0){
-      sb.append(value%2);
-      value/=2;
+    public String getNextBinary(String binary){
+        //제거할 0의 개수 구하기
+        int counter = 0;
+        int len = binary.length();
+        
+        for(int i = 0; i < len; i++){
+            if(binary.charAt(i) == '0'){
+                counter++;
+            }
+        }
+        //총합 구하기
+        removedCounter+=counter;
+        //현재 s길이에서 제거할 0의 개수를 뺸 것을 2진수로 변환하기.
+        return getBinary(len-counter);
+        
     }
-    sb.reverse();
-
-
-    return sb.toString();
-
-  }
+    private String getBinary(int number){
+        //2진수 변환
+        StringBuilder sb = new StringBuilder();
+        while(number!=0){
+            sb.append(String.valueOf(number%2));
+            number/=2;
+        }
+        return sb.reverse().toString();
+    }
 }
