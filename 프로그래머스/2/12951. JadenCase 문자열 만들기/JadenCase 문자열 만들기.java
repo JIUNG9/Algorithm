7ed3f3b7ae62074d50ef37ddf39a92a6a5533ae3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-26 11:58:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.14 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,44 +1,28 @@
 import java.util.*;
 class Solution {
     public String solution(String s) {
-        int len = s.length();
-        StringBuilder sb = new StringBuilder();
-        boolean isFirstLetterAfterWhiteSpace = true;
+        //모든 문자열을 순회하는데 공백이 아닌 것이 나오는 순간의 첫 순간에 해당 문자열이 영어라면 대문자로 수정 첫 순간이 아닌 것은 모두 소문자
+        StringBuilder sb = new StringBuilder(); 
+        boolean isFirstLetter = true;
         
-        for(int i = 0; i < len; i++){
-            while(i < len && s.charAt(i)==' '){
-                sb.append(s.charAt(i++));
-                isFirstLetterAfterWhiteSpace = true;
+        for(int i = 0; i < s.length(); i++){
+            while(i < s.length() && s.charAt(i) == ' '){
+                isFirstLetter = true;
+                sb.append(s.charAt(i));
+                i++;
             }
-            if(i < len){
-                if(isFirstLetterAfterWhiteSpace && isLetter(s.charAt(i))){
-                    isFirstLetterAfterWhiteSpace = false;
-                    sb.append(changeToUpper(s.charAt(i)));
-                }
-                else {
-                    sb.append(changeToLowerCase(s.charAt(i)));
-                    isFirstLetterAfterWhiteSpace = false;
-                }
+            if(i == s.length()) break;
+            if(isFirstLetter){
+                sb.append(Character.toUpperCase(s.charAt(i)));
+                isFirstLetter= false;
+            }
+            else{
+                sb.append(Character.toLowerCase(s.charAt(i)));
             }
             
         }
-
-       
-            
-            return sb.toString();
-        }
-
-    public boolean isLetter(char c){
-        return Character.isLetter(c);
-    }
-    
-    public char changeToUpper(char c){
-        return Character.toUpperCase(c);
-    }
-    
-    public char changeToLowerCase(char c){
-        return Character.toLowerCase(c);
-    }
-    
-    }
-
+        return sb.toString();
+      
+      
+}
+}
