524b298b439f74c14e956489a8219ab7416abf95
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-27 09:09:17 +0900

    [level 2] Title: 숫자의 표현, Time: 1.50 ms, Memory: 51.9 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,26 +1,20 @@
 class Solution {
     private int counter = 0;
     public int solution(int n) {
-        //만약 현재 sum이 n보다 작다면 right pointer를 증가시켜 sum에 더한다.
-        //만약 현재 sum이 n보다 크다면 left pointer를 증가시켜 sum을 뺀다.
-        //left와 right가 같아질 때까지 반복한다.
-        int left = 1;
-        int right = 1;
-        int sum = 0;
-        while(left <= right && left <= n){
-            if(sum == n) 
-            {
-                counter++; 
-                sum-=left++;
+        //1부터 n까지 연속된 숫자를 시작 수 부터 n까지 총합이 n보다 작을 때까지 더한다. 만약 총합이 n을 넘는다면 종료한다. 그리고 값이 일치한다면 경우의 수를 추가한다.
+        
+        for(int i = 1; i <= n; i++){
+            int sum = 0;
+            //copied i, i의 idx를 움직이지 않는다. i는 outernal roation 숫자이다.
+            int j = i;
+            //범위가 허용하는 한 n보다 작을 때까지 더한다.
+            while(j <= n && sum < n){
+                sum+=j;
+                j++;
             }
-            else if(sum < n){
-                sum+=right++;
-            }
-            else{
-                sum-=left++;
-            }
+            if(sum == n) counter++;
         }
-            return counter;
+        return counter;
         
     }
 }
