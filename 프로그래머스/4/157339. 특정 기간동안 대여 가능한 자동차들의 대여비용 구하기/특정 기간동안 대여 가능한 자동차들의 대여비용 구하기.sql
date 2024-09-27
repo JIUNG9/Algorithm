f3015f5181ace1bf3d8ec9bc1e5c77512151fee0
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-27 21:59:24 +0900

    [level 4] Title: 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
+++ 프로그래머스/4/157339. 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기/특정 기간동안 대여 가능한 자동차들의 대여비용 구하기.sql
@@ -1,21 +1,16 @@
 -- 코드를 입력하세요
-with get_specific_type as (
-    select car_id, car_type, daily_fee
-    from  car_rental_company_car
-    where car_type like "세단" OR car_type like "SUV"
-)
-, filter_date as (
-    select car_id
-    from car_rental_company_rental_history
-    where END_DATE >= '2022-11-01'
+-- 도메인 : history에는 같은 차량 번호가 존재할 수 있다. 빌릴 수 있는 차량을 조회해도
+-- 같은 차량 넘버로 11월 1일이나 11월 30일에 빌릴 수도 있다.
+
+WITH can_not_borrow AS (
+    SELECT car_id
+    FROM car_rental_company_rental_history
+    WHERE NOT (start_date > '2022-11-30' OR end_date < '2022-11-01')
 )
 
-# select *
-# from filter_date
-
-
-select a.car_id, a.car_type, round(daily_fee * (1-(discount_rate* 0.01)),0) * 30 as fee
-from get_specific_type as a inner join CAR_RENTAL_COMPANY_DISCOUNT_PLAN as b on a.car_type = b.car_type
-where duration_type like "30%" AND car_id NOT IN(select car_id from filter_date)
-having fee >= 500000 AND fee < 2000000
-order by fee desc, a.car_type asc, a.car_id desc
+select car_id, crcc.car_type, 30 * round(daily_fee / 100 * (100 - discount_rate),0)as fee 
+from car_rental_company_car as crcc inner join car_rental_company_discount_plan as crcd on crcc.car_type = crcd.car_type
+where (crcc.car_type like 'SUV' or crcc.car_type like '세단') and duration_type like "30일%" and car_id not in (select car_id from can_not_borrow)
+group by car_id
+having fee >= 500000 and fee < 2000000
+order by 3 desc, 2 asc, 1 desc
