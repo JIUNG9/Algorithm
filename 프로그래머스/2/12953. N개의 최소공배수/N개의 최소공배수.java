015b7996e2758f19e78ab69e7f653aa13840729c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-29 08:36:52 +0900

    [level 2] Title: N개의 최소공배수, Time: 33.59 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,35 +1,32 @@
 import java.util.*;
-import java.util.stream.*;
-
 class Solution {
     public int solution(int[] arr) {
         
-        List<Integer> sortedList = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
+        //arr의 원소의 끝에 도달할 때까지 arr을 정렬하여, 첫 원소부터 2개의 원소의 최소공배수를 구하여 현재 검사하는 원소를 두 수의 최소공배수로 업데이트하고 그 다음 원소와 다시 공약수를 구하여 최소공배수를 구하여 반복한다. 최종적으로 존재하는 값이 모든 수의 최소공배수이다.
+        Arrays.sort(arr);
+        for(int i = 1; i < arr.length; i++){
+            //최소공배수
+            //두 수의 공약수들 중 가장 큰 수를 얻어 arr의 원소들을 나눈 값과 공약수들 중 가장 큰 값을 구한다.
+            List<Integer> cdList = getCD(arr[i-1], arr[i]);
+            int lcd = cdList.get(cdList.size()-1);    
+            
+            
         
-        while(sortedList.size() > 1){
-            int first = sortedList.get(0);
-            int second = sortedList.get(1);
-            
-            int gcb = getGcb(first,second);
-            int lcm = (first/gcb) * second;
-            
-            sortedList.add(lcm);    
-            sortedList.remove(0);
-            sortedList.remove(0);
-
+            arr[i] = lcd * (arr[i-1]/lcd) * (arr[i]/lcd);
         }
-        return sortedList.get(0);
-        
-        
+        return arr[arr.length-1];
     }
-    public int getGcb(int a, int b){
-        int gcb = 1;
-        for(int i = 1; i <= a; i++){
-            if(a%i == 0 && b%i == 0 && gcb < i){
-                gcb = i;
+    public List<Integer> getCD(int min, int max){
+        //공약수를 구하는 법은 1부터 작은 수를 기준으로 min까지 모두 양쪽을 나누어 둘 다 나누어 떨어지는 것이 공약수이다.
+        List<Integer> cd = new ArrayList<>();
+        for(int i = 1; i <= min ; i++){
+            if(min%i == 0 && max % i == 0)
+            {
+                System.out.println("cd: " + i);
+                cd.add(i);
             }
         }
-        return gcb;
-        
+        return cd;
     }
+    
 }
--- 프로그래머스/2/12953. N개의 최소공배수/README.md
+++ 프로그래머스/2/12953. N개의 최소공배수/README.md
@@ -2,26 +2,49 @@
 
 [문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12953) 
 
-### Approach
+### 성능 요약
 
-- Get the least common multiple from two of input until there is a one element in list, For getting the least common multiple, get the gcd.
-- For getting gcd, get the max value from 1 to under the smaller input value which is remainder operation result is 0.
-- And remove the two of input and insert the current least common multiple value
+메모리: 87.3 MB, 시간: 33.59 ms
 
-```
-list = Arrays.stream(arr).boxed().sorted().collect(Collectors.toList());
-while(list.size > 1 )
-int first = list.get(0);
-int second = list.get(1);
-int gcd = getGcd(first, second);
-int lcm = first / gcd * second;
-list.remove(0);
-list.remove(0);
-list.add(lcm);
+### 구분
 
+코딩테스트 연습 > 연습문제
 
-return list.get(0);
-```
+### 채점결과
 
+정확성: 100.0<br/>합계: 100.0 / 100.0
+
+### 제출 일자
+
+2024년 09월 29일 08:36:51
+
+### 문제 설명
+
+<p>두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다. 예를 들어 2와 7의 최소공배수는 14가 됩니다. 정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다. n개의 숫자를 담은 배열 arr이 입력되었을 때 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요. </p>
+
+<h5>제한 사항</h5>
+
+<ul>
+<li>arr은 길이 1이상, 15이하인 배열입니다.</li>
+<li>arr의 원소는 100 이하인 자연수입니다.</li>
+</ul>
+
+<h5>입출력 예</h5>
+<table class="table">
+        <thead><tr>
+<th>arr</th>
+<th>result</th>
+</tr>
+</thead>
+        <tbody><tr>
+<td>[2,6,8,14]</td>
+<td>168</td>
+</tr>
+<tr>
+<td>[1,2,3]</td>
+<td>6</td>
+</tr>
+</tbody>
+      </table>
 
 > 출처: 프로그래머스 코딩 테스트 연습, https://school.programmers.co.kr/learn/challenges
