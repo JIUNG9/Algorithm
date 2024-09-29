062dada3e1f1081f4a2eb9aac1be35b1c46b357c
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-30 06:12:46 +0900

    [level 2] Title: n^2 배열 자르기, Time: 408.99 ms, Memory: 473 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.java
@@ -1,29 +1,29 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int[] solution(int n, long left, long right) {
+        //0번째 행은 1이 1개, 1번째 행은 2가 2개, 2번쨰 행은 3이 3개이다. 이후 각 행에는 다음에 올 숫자부터 n까지의 원소가 온다. i번째 행은 i+1을 i+1개만큼 집어넣고 이후 i+2부터 n까지의 값을 집어 넣는다.
+        //모든 행과열의 경우의 수를 세게 된다면 10^14이므로 해당 행에 속하는 경우의 수만 고른다.
         
-        //10^7^2를 통해 모든 정사각형의 case를 구하는 것은 시간 초과가 나므로, 시작 row부터 끝 row까지 원소를 구한 뒤 원하는 원소만 출력
-        //Impl: 시작row는 left에서 n을 나눈 것 부터 시작, 끝row는 나눈 것에서 + 1
-        //row를 모은 이후 시작점은 left % n, end row쪽은 right보다 더 많은 원소를 담았으므로 담은 원소들의 길이에서 right를 뺀 것의 길이만큼 제외하고 
-        //left부터 right를 담는다.
-        //0번째 row이면 1이 1개 이고 n까지, 1번째 row이면 2가 2개이고 n까지, 2번째 row이면 3이 3개이고 n까지
-        //row의 크기가 증가할 때 마다 넣어야하는 반복 수와 숫자가 같다, 이후 끝난 숫자부터 n까지 연속적인 숫자를 입력
-        int startRow = (int)(left/n);
-        int endRow = (int)(right/n);
-        int totalLen = ((endRow - startRow) * n);
-        int startIdx = (int)(left % n);
-        int endIdx = startIdx +(int)(right - left);
+        //left에는 나머지 연산자 값 부터 right는 전체 사이즈에서 - (n - 나머지 연산자) 값 까지의 값을 구하여 해당 sub array를 반환한다.
+        
+        int ld = (int)(left / n);
+        int rd = (int)(right / n);
         List<Integer> list = new ArrayList<>();
-        
-        for(int i = startRow; i <= endRow; i++){
-            for(int j = 0; j < i+1; j++){
-                list.add(i+1);        
+        for(int i = ld; i <= rd; i++){
+            for(int k = 0; k < i + 1; k++){
+                list.add(i+1);
             }
-            for(int k = i + 2; k <= n; k++){
-                list.add(k);
+            for(int z = i + 2; z <= n; z++){
+                list.add(z);
             }
         }
-        return list.subList(startIdx,endIdx+1).stream().mapToInt(i->i).toArray();
+        // for(Integer i : list){
+        //     System.out.println("i :" + i);
+        // }
+        
+        
+        return list.subList((int)(left%n), (list.size() + 1) - (n - (int)(right % n))).stream().mapToInt(i ->i).toArray();
         
     }
 }
