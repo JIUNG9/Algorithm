d02f211f7428eff4b1b398d05347d208d4ed3ecc
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-29 17:24:32 +0900

    [level 2] Title: 할인 행사, Time: 99.08 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,42 +1,38 @@
 import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    private Map<String,Integer> wantedMap;
-    private int counter = 0;
+    private Map<String,Integer> map;
     public int solution(String[] want, int[] number, String[] discount) {
+        //특정날 부터 10일동안 해당 want에 존재하는 number가 모두 존재한다면 그 날은 사용자가 원하는 개수만큼의 제품이 모두 할인한다는 것을 의미한다.
+        //구현 첫 날부터 모든 날까지 10일동안 want와 number를 각각  key,value로 설정한 Map을 만들고 특정날부터 10일동안 해당 number가 모두 0이되는지 확인하여 모두 0이라면 해당 날은 되는 날로 숫자를 세고 아니라면 안 되는날로 센다.
         
-        initMap(want,number);
-        int discountLen = discount.length;
-    
-        for(int i = 0; i <  discountLen; i++){
-            if(wantedMap.containsKey(discount[i])){
-                    int value = wantedMap.get(discount[i]);
-                    wantedMap.put(discount[i], value - 1);
-            } 
-            if(i > 9){
-                String beforeBargained = discount[i-10];
-                if(wantedMap.containsKey(beforeBargained)){
-                    wantedMap.put(beforeBargained, wantedMap.get(beforeBargained)+1);
+        int answer = 0;
+     
+        for(int j =0; j < discount.length; j++){
+            //Should init the Map every time. even point to cache Map, It will use the reference of the cache Key and Value
+            initMap(want,number);
+            //The second problem is set the k with condition with this for loop
+            //by iterating this loop k will be increased so using the K with condition will scan all of element which is smaller than discount.length
+            for(int k = j; k < j + 10 && k < discount.length; k++){
+            String key = discount[k];
+            if(map.containsKey(key)){
+                int value = map.get(key) - 1;
+                if(value == 0) map.remove(key);
+                else{
+                    map.put(key,value);
                 }
-                }
-            if(isMapValuesAllBargained()) {
-                counter++;
             }
         }
-        return counter;
-      
-    }
-    public boolean isMapValuesAllBargained(){
-        return wantedMap.values().stream().filter(i -> i > 0).count() > 0 ? false: true;
-    }
-    public void initMap(String[] want, int[] number){
-
-        wantedMap = new HashMap<>();
-        for(int i = 0; i < want.length; i++){
-            wantedMap.put(want[i], number[i]);
+            if(map.size()==0) {
+                answer++;
+            }
         }
+        return answer;
     }
     
-
+    public void initMap(String[] want, int[] number){
+        map = new HashMap();
+        for(int i = 0; i < want.length; i++){
+            map.put(want[i],number[i]);
+        }
+    }
 }
