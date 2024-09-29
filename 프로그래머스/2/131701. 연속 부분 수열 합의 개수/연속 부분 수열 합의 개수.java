c62633dfe9cb363a2c3a1422b6e6bde834e77170
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-29 13:33:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 143.69 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,23 +1,23 @@
 import java.util.*;
-
 class Solution {
     Set<Integer> set;
     public int solution(int[] elements) {
-        int len = elements.length;
-         set = new HashSet<>(); 
-        for(int i = 0; i < len; i++){
-            int sum = 0;
-            for(int j = i; j < len + i; j++){               
-                sum+=elements[j % len];
-                set.add(sum);
-            }
-        }
+        //순서를 지켜 숫자를 더해서 나올 수 있는 경우의 수를 중복 없이 저장하여 해당 개수를 반환한다.
+        //dfs -> Heap에 어떤 것을 사용할 것인지, Stack은 어떻게 사용할 것인지, 순서는 상관있는지(동작)
+        
+        set = new HashSet<>();
+        dfs(elements, 0, 0, elements.length);
+        
         return set.size();
     }
-    public void sout(){
-        for(Integer i : set){
-            System.out.println("i: " + i);    
-        }
+    
+    public void dfs(int[] e, int idx, int sum, int len){
         
+        for(int i = idx; i < len + idx; i++){
+            sum+=e[i%len];
+            set.add(sum);
+            
+        }
+         if(idx < e.length) dfs(e, idx + 1, 0 , len);
     }
 }
