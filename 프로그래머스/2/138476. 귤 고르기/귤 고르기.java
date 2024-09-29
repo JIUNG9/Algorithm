4cad08c2952b2fe2737e5c9bcc41a69e387c00e6
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-29 12:27:40 +0900

    [level 2] Title: 귤 고르기, Time: 92.05 ms, Memory: 94.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,29 +1,30 @@
 import java.util.*;
 import java.util.stream.*;
-
 class Solution {
-    private int counter = 0;
-    private Map<Integer,Integer> map;
     public int solution(int k, int[] t) {
-        map = new HashMap<>();
-        
+        //종류의 개수가 최소가 되게 t에서 k개를 뽑아 귤의 최소의 종류의 값을 반환
+        //각 종류별 개수를 구하여 내림 차순 정렬한 후 k가 0이 될 때까지 뺀다.
+        Map<Integer,Integer> map = new HashMap<>();
+        int answer = 0;
         for(int i = 0; i < t.length; i++){
-            map.put(t[i],map.getOrDefault(t[i],0)+1);
+            int key = t[i];
+            if(map.containsKey(key)){
+                map.put(key, map.get(key) + 1);
+            }
+            else{
+                map.put(key, 1);
+            }
         }
         
-        List<Integer> valuesDesc = map.values().stream().sorted((i1,i2)->{
-            return Integer.compare(i2,i1);
-        }).collect(Collectors.toList());
+        
+    List<Integer> amount = map.values().stream().sorted(Collections.reverseOrder()).collect(Collectors.toList());
         
         
-        for(Integer i: valuesDesc){
-            if(k <= 0) break;
-            k-=i;
-            counter++;
-        }    
-            
-        return counter;
-            
-    
+            for(Integer i : amount){
+                if(k <= 0) break;
+                k-=i;
+                answer++;
+            }
+        return answer;
     }
 }
