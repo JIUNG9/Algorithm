a0c9deeae75648955592a8d5a8346530a331de25
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-09-30 17:21:11 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,26 +1,23 @@
 import java.util.*;
 class Solution {
-     
-     public int solution(String[][] clothes) {
-    int len = clothes.length;
-    Map<String, Integer> map = new HashMap<>();
-    int cases = 1;
-
-    //save into Map
-    for (int i = 0; i < len; i++) {
-      map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+    public int solution(String[][] clothes) {
+        
+        //의상 종류별 개수를 구하고 의상의 종류가 한 개라면 해당 종류에 대한 개수를 반환
+        //아니라면 종류별 개수들의 곱의 합에서 1개씩만 골라서 입을 수 있는 경우의 수를 즉 모든 값(values)의 총 합을 구한다.(아예 입지 않는 경우의 수 제외이므로)
+        
+        Map<String, Integer> map = new HashMap<>();
+        int sum = 1;
+        
+        for(int i = 0; i < clothes.length; i++){
+            String key = clothes[i][1];
+            map.put(key,map.getOrDefault(key,0)+1);
+        }
+        
+        for(Integer i : map.values()){
+            sum*=(i+1);
+        }
+    
+        return sum -1;
+        
     }
-
-
-    for (int i = 0; i < len; i++) {
-      if (map.containsKey(clothes[i][1])) {
-        cases *= (map.get(clothes[i][1])+1);
-        map.remove(clothes[i][1]);
-      }
-    }
-
-    return cases -1 ;
-
-
-  }
 }
