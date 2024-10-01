a9a65fe0bbce8f86509de61fa2019803dbe67c4f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-01 18:51:26 +0900

    [level 2] Title: 전화번호 목록, Time: 357.67 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,12 +1,13 @@
 import java.util.*;
 class Solution {
-    public boolean solution(String[] arr) {
+    public boolean solution(String[] pb) {
+        //String은 정렬하면 lexically하게 정렬된다. a, ab, abc 이렇게 정렬된다. 따라서 현재 확인하는 문자열 이후의 문자열이 현재 문자열을 포함한다면 return false이다.
         
-        Arrays.sort(arr);
-        int len = arr.length;
-        
-        for(int i = 1; i < len; i++){
-            if(arr[i].startsWith(arr[i-1])) return false;
+        Arrays.sort(pb);
+        for(int i = 0; i < pb.length -1; i++){
+            String first = pb[i];
+            String second = pb[i+1];
+            if(second.startsWith(first)) return false;
         }
         return true;
     }
