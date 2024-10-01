08b71d3fd587050a4a4b3182e68437392bb6ac6f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-01 18:31:05 +0900

    [level 2] Title: 튜플, Time: 120.66 ms, Memory: 99.9 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -1,42 +1,33 @@
 import java.util.*;
-import java.util.stream.*;
 import java.util.regex.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(String s) {
-
-        //원소의 개수가 가장 적은 리스트를 순차적으로 정답에 담아 반환한다.
-        
-        //첫 문자와 마지막 문자를 제외하고 {.} 로 split 하여 각 배열을 얻는다. 그렇게 한다면 {}를 제외한 각 리스트의 원소들을 합한 문자열을 얻을 수 있다. 예시로는 {1,2,3}은 1,2,3이 된다. 해당 문자열을 ,로 스플릿하고 해당 문자열을 List<String[]>에 담는다. 해당 List에 있는 String 배열을 길이 기준으로 오름차순 정렬하여, List를 모두 순회하여 List의 0번째는 0번째 원소, 1번 째는 0번째에서 가지고 있지 않은 문자열, 
-        int len = s.length();
-        List<String[]> stringList = new ArrayList<>();
-        List<Integer> answerList = new ArrayList<>();
-  
-        s = s.substring(1,len-1);
-            
-        String[] removeBracketStr = s.split("[{}]");
-        
-        for(String str : removeBracketStr){
-            if(str.equals("") || str.equals(",")) continue;
-            String[] removedSeparator = str.split(",");
-            for(String removedStr : removedSeparator){
-            }
-            stringList.add(removedSeparator);
+        //{number, number}-> {} 안에 있는 한 개 혹은 여러 개의 숫자람녀 숫자와 , 조합을 그룹하여 얻어야함
+        //1개, 2개, 3개 .. n개까지가 있는 원소들을 얻고, 개수별로 오름차순하여 정렬한 것에 1개 부터 존재하지 않는 것만 결과값에 집어넣고 해당 결과를 반환한다.
+        Pattern pattern = Pattern.compile("\\{([0-9,]+)\\}");
+        Matcher matcher = pattern.matcher(s);
+        List<String> answerList = new ArrayList<>();
+        List<String[]> list = new ArrayList<>();
+        while(matcher.find()){
+            String str = matcher.group(1);
+            String[] arr = str.split(",");
+            list.add(arr);
         }
-        
-        stringList = stringList.stream().sorted((s1Arr,s2Arr)->{
-            return s1Arr.length - s2Arr.length;
+        list = list.stream().sorted((list1,list2) -> {
+            return Integer.compare(list1.length, list2.length);
         }).collect(Collectors.toList());
         
-        for(int i = 0; i < stringList.size();i++){
-            String[] strArr = stringList.get(i);
-            for(String strArrElement : strArr){
-                int element = Integer.parseInt( strArrElement) ;
-                if(!answerList.contains(element)) answerList.add(element); 
+        for(String[] arrStr : list){
+            for(int i = 0; i < arrStr.length; i++){
+                
+                if(!answerList.contains(arrStr[i])){
+                  answerList.add(arrStr[i]);
+                } 
             }
         }
-        return answerList.stream().mapToInt(i->i).toArray();
-            
-            
+        return answerList.stream().mapToInt(i->Integer.parseInt(i)).toArray();
+       
     }
 }
