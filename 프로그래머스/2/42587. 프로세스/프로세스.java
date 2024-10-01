c4e044455f08ceb486e98e85557c76e3b8902407
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-01 11:36:02 +0900

    [level 2] Title: 프로세스, Time: 10.67 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,60 +1,43 @@
 import java.util.*;
 class Solution {
+    public int solution(int[] priorities, int location) {
+
+        //현재 확인하는 작업의 idx가 location과 일치하고  처리해야하는 작업의 우선 순위와 일치한다면 현재까지 처리한 작업의 개수 + 1을 반환한다. 아니라면 현재 확인하는 작업의 우선순위와 처리해야하는 작업 순위가 동일하다면 해당 작업을 처리하고 처리한 작업의 개수를 1 증가시키고 아니라면 해당 작업을 뽑아 가장 맨 마지막으로 넣는다.
+        //-> 수정(함축) 현재 확인하는 작업(poll)이 우선순위와 일치하다면 우선순위 작업 큐에서 해당 작업을 처리하고 이때(Inner for loop) idx가 location과 일치하다면 정답을 반환
+        
+        int counter = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        Queue<P> q = new LinkedList<>();
+        
+        for(int i = 0; i < priorities.length; i++){
+            pq.add(priorities[i]);
+            q.add(new P(i,priorities[i]));
+        }
+        while(!q.isEmpty()){
+            P p = q.poll();
+                if(p.priority == pq.peek()) 
+                {
+                    counter+=1;
+                    pq.poll();
+                    if(p.idx == location) return counter;
+
+                }
+                else{
+                    q.add(p);
+                }
+            }
+        return counter;
+
+        }
+        
     
-    private int counter = 0;
-    private Queue<Task> tasks;
-    private Queue<Integer> q;
-
-
-  public int solution(int[] priorities, int location) {
-
-    tasks = new LinkedList<>();
-    q = new PriorityQueue<>(Collections.reverseOrder());
-    init(priorities);
-
-    while (!q.isEmpty() && !tasks.isEmpty()) {
-      Task task = tasks.poll();
-      if (task.idx == location && task.priority == q.peek()) {
-        counter++;
-        break;
-      } else if (task.priority == q.peek()) {
-        counter++;
-        q.poll();
-      } else {
-        tasks.add(new Task(task.idx, task.priority));
-      }
+    class P{
+        private int idx;
+        private int priority;
+        
+        public P(int idx, int priority){
+            this.idx = idx;
+            this.priority = priority;
+        }
     }
-    return counter;
-
-  }
-
-  public void init(int[] priorities) {
-    int len = priorities.length;
-
-    for (int i = 0; i < len; i++){
-      Task task = new Task(i, priorities[i]);
-      q.add(priorities[i]);
-      tasks.add(task);
-    }
-
-  }
-
-}
-
-class Task {
-
-
-  int idx;
-  int priority;
-
-  public Task(int idx, int priority) {
-    this.idx = idx;
-    this.priority = priority;
-  }
-
-  public String toString() {
-    return "this idx : " + idx + " ," + "this priority : " + priority;
-
-
-  }
 }
