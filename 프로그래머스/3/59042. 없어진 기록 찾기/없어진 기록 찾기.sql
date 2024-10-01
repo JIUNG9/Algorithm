782b329a2e9ca639c3c5b2b6b0a9aefe66cc7a68
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-01 20:11:59 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,8 +1,6 @@
 -- 코드를 입력하세요
--- Domain : animal_out에는 있지만 animal_in에는 없는 ao의 animal_id 와 ao의 name
--- Impl : right outer join
-
+-- left outer join
 select ao.animal_id, ao.name
 from animal_ins as ai right join animal_outs as ao on ai.animal_id = ao.animal_id
 where ai.animal_id is null
-order by 1 asc
+order by 1,2
