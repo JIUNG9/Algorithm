f73e7ce08b9f8ef359529b02bfd297a5f252d3f2
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 08:53:17 +0900

    [level 2] Title: 방문 길이, Time: 3.51 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -5,6 +5,7 @@ class Solution {
     public int solution(String dirs) {
         
         //사용자가 5 * 5을 지시 방향대로 움직이는데, 움직이는 곳이 맵을 초과한다면 아무 행동도 하지 않고, 만약 초과하지 않았다면 해당 위치로 움직이고 이미 방문한 적이 없는 위치라면 해당 위치를 기록한다. 처음 방문하는 길의 루트 (from + to), (to + from이 같은 것으로 작용한다.)
+        //Feed -> 문제 좀 더 꼼꼼히 보기. 테스트 케이스를 좀 더 잘 살펴보면 됐었다.
         int currentX = 0;
         int currentY = 0;
         Set<String> set = new HashSet<>();
