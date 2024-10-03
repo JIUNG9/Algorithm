43253d077ba4d5c10d114e8c36179d02b79fd69d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 08:50:14 +0900

    [level 2] Title: 방문 길이, Time: 2.88 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,112 +1,46 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Objects;
-
-
+import java.util.*;
 class Solution {
- private int counter = 0;
-  private Map<String, int[]> move;
-  private Set<Person> set;
-
-
-
-  public int solution(String dirs) {
-    String[] arr = dirs.split("");
-    int len = arr.length;
-    move = new HashMap<>();
-    set = new HashSet<>();
-    initMap();
-    Person p = new Person(0, 0, 0, 0);
-
-    for (int i = 0; i < len; i++) {
-      String dir = arr[i];
-      int[] newPosition = move.get(dir);
-      int newX = newPosition[0];
-      int newY = newPosition[1];
-      int willBeMovedX = p.currentX + newX;
-      int willBeMovedY = p.currentY + newY;
-      p = new Person(willBeMovedX,willBeMovedY,p.currentX,p.currentY);
-      Person reversedP = new Person(p.currentX,p.currentY,willBeMovedX,willBeMovedY);
-      if (isRangeSatisfied(willBeMovedX, willBeMovedY) && !set.contains(p) && ! set.contains(reversedP)) {
-        set.add(new Person(willBeMovedX, willBeMovedY, p.currentX, p.currentY));
-        set.add(new Person(p.currentX, p.currentY,willBeMovedX, willBeMovedY));
-        counter++;
-      }
-      if(isRangeSatisfied(willBeMovedX,willBeMovedY)){
-        p.updateX(willBeMovedX);
-        p.updateY(willBeMovedY);
-      }
-
-
+    private Map<String, int[]> map;
+    private int answer = 0;
+    public int solution(String dirs) {
+        
+        //사용자가 5 * 5을 지시 방향대로 움직이는데, 움직이는 곳이 맵을 초과한다면 아무 행동도 하지 않고, 만약 초과하지 않았다면 해당 위치로 움직이고 이미 방문한 적이 없는 위치라면 해당 위치를 기록한다. 처음 방문하는 길의 루트 (from + to), (to + from이 같은 것으로 작용한다.)
+        int currentX = 0;
+        int currentY = 0;
+        Set<String> set = new HashSet<>();
+        map = new HashMap<>();
+        initMap();
+        
+        for(int i = 0; i < dirs.length(); i++){
+            String s = String.valueOf(dirs.charAt(i));
+            int[] d = map.get(s);
+            int movedX = currentX +  d[1];
+            int movedY = currentY +  d[0];
+            if(!isMapExceeded(movedY,movedX)){
+                String from = String.valueOf(currentY) + String.valueOf(currentX);
+                String to = String.valueOf(movedY) + String.valueOf(movedX);
+                if(!set.contains(from+to) && !set.contains(to+from)){
+                    set.add(from+to);    
+                    set.add(to+from);
+                    answer++;
+                }
+                currentX = movedX;
+                currentY = movedY;
+            }
+        }
+        return answer;
+        
+        
     }
-
-    return counter;
-
-  }
-
-
-  public boolean isRangeSatisfied(int willBeMovedX, int willBeMovedY) {
-    if (willBeMovedX >= -5 && willBeMovedY >= -5 && willBeMovedX <= 5 && willBeMovedY <= 5) {
-      return true;
+    public void initMap(){
+        map.put("U", new int[]{1,0});
+        map.put("L", new int[]{0,-1});
+        map.put("R", new int[]{0,1});
+        map.put("D", new int[]{-1,0});
     }
-    return false;
-
-
-  }
-
-  public void initMap() {
-    move.put("U", new int[]{0, 1});
-    move.put("D", new int[]{0, -1});
-    move.put("R", new int[]{1, 0});
-    move.put("L", new int[]{-1, 0});
-  }
-
-  class Person {
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (!(o instanceof Person)) {
+    
+    public boolean isMapExceeded(int y, int x){
+        if(y < -5 || x < -5 || y > 5 || x > 5) return true;
         return false;
-      }
-      Person person = (Person) o;
-      return willBeMovedX == person.willBeMovedX && willBeMovedY == person.willBeMovedY
-          && currentX == person.currentX && currentY == person.currentY;
     }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(willBeMovedX, willBeMovedY, currentX, currentY);
-    }
-
-    int willBeMovedX;
-    int willBeMovedY;
-    int currentX;
-    int currentY;
-
-    public Person(int willBeMovedX, int willBeMovedY, int currentX, int currentY) {
-      this.willBeMovedX = willBeMovedX;
-      this.willBeMovedY = willBeMovedY;
-      this.currentX = currentX;
-      this.currentY = currentY;
-
-    }
-
-    public void updateX(int x) {
-      this.currentX = x;
-    }
-
-    public void updateY(int y) {
-      this.currentY = y;
-    }
-
-
-
-
-  }
-
-  }
+}
