4c07e614debf4f62bcf3adc0f4bf21e1280b5869
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-03 21:57:17 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,6 +1,6 @@
 -- 코드를 입력하세요
+-- 입양을 못 간 동물을 이름과 데이트타임을 3개까지 표현 -> in은 했지만 out을 하지 못 한 것, left outer join을 하면 된다.
 select ai.name, ai.datetime
 from animal_ins as ai left join animal_outs as ao on ai.animal_id = ao.animal_id
-where ao.animal_id is null
-order by ai.datetime asc limit 3
--- Impl: 아직 입양을 가지 못한 동물을 구하기 위해 Ins에는 존재하지만 Outs에는 존재하지 않는 row를 조회하기 위해 left outer  join을 통해 입양을 가지 못한 동물을 구하고 ins의 입양 date time을 desc하여 3개의 row를 얻어, 이름과 보호시작을 반환한다.
+where ao.animal_id is null 
+order by 2 asc limit 3
