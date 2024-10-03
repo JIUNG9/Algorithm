9eb80b9ead3cb4de7c2be654d1dace262498aebc
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-03 19:30:55 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,33 +1,34 @@
--- 코드를 입력하세요
--- Domain: Online Sale에는 같은 유저 아이디가 존재할 수 있다.
--- 2021년에 가입한 전체의 유저의 회원 아이디를 기준으로 구매한 유저의 수와 2021년의 가입한 전체 회원의 수와 구매한 사람의 비율을 구하여라 online sale에 존재하는 유저 아이디가 여러 개라면 한 개만 포함하여 고려한다.비율은 소수 두 번째 자리에서 반올림하고 년 별, 월 별을 구하고 년, 월 기준으로 오름차순한다.
+# -- 코드를 입력하세요
+# -- 2021년에 가입한 회원id를 기준으로 조인하여, online_sale을 회원 id로 집계한다. 이를 통해 한 회원이 여러 차례의 상품을 구매한 것을 방지할 수 있다.(online_sale에는 한 개 이상의 회원 아이디가 존재할 수 있다.), 같은 년, 월에 회원이 두 번 들어가면 안 된다.
 
-
-with get_the_number_user_sign_up_2021 as (
-    select user_id
-    from user_info
-    where year(joined) like '2021'
-)
-
--- see the difference between two of that  round(count(distinct os.user_id) /count(us.user_id),1)as PUCHASED_RATIO vs  round(count(distinct os.user_id) /(select count(user_id) from get_the_number_user_sign_up_2021) ,1) as PUCHASED_RATIO. The first one means the number of the user which buy the products at 2021 which is grouped by the year and month. otherwsie second one is not. Just the number of user who signed up at 2021
-
-
-select year(sales_date)as YEAR, month(sales_date)as MONTH , count(distinct os.user_id) as PURCHASED_USERS, round(count(distinct os.user_id) /(select count(user_id) from get_the_number_user_sign_up_2021),1)as PUCHASED_RATIO
-from  get_the_number_user_sign_up_2021 as us inner join online_sale as os on us.user_id = os.user_id
-group by 1,2
-order by 1,2
-
-
-# WITH JOINED2021 AS (
-#     SELECT  *
-#     FROM    USER_INFO
-#     WHERE   YEAR(JOINED) = '2021'
+# with get_user_2021 as(
+#     select user_id
+#     from user_info u
+#     where year(u.joined) = '2021'
+# )
+# , get_total_cnt as(
+#     select count(user_id) as cnt
+#     from user_info u
+#     where year(u.joined) = '2021'
 # )
 
-# SELECT      YEAR(B.SALES_DATE) YEAR, MONTH(B.SALES_DATE) MONTH,
-#             COUNT(DISTINCT B.USER_ID) PURCHASED_USERS,
-#             ROUND(COUNT(DISTINCT B.USER_ID) / (SELECT COUNT(*) FROM JOINED2021), 1) PURCHASED_RATIO
-# FROM        JOINED2021 A
-# INNER JOIN  ONLINE_SALE B ON A.USER_ID = B.USER_ID
-# GROUP BY    YEAR, MONTH
-# ORDER BY    YEAR, MONTH
+
+
+
+
+# select year(sales_date) as year, month(sales_date) as month, count(distinct os.user_id) as purchased_users, round((count(distinct(os.user_id))/cnt) ,1) as puchased_ratio
+# from  get_user_2021 gu inner join online_sale os , get_total_cnt
+# group by 1,2
+# order by 1,2
+
+SELECT
+    YEAR(SALES_DATE) AS YEAR,
+    MONTH(SALES_DATE) AS MONTH,
+    COUNT(DISTINCT USER_ID) AS PURCHASED_USERS,
+    ROUND(COUNT(DISTINCT USER_ID)/(SELECT COUNT(USER_ID)
+     FROM USER_INFO
+     WHERE YEAR(JOINED) = 2021), 1) AS PURCHASED_RATIO
+FROM USER_INFO INNER JOIN ONLINE_SALE USING(USER_ID)
+WHERE YEAR(JOINED) = 2021
+GROUP BY YEAR, MONTH
+ORDER BY YEAR ASC, MONTH ASC;
