191a7ec76f4791488566d0f074771d6480ae1433
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-03 18:54:40 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,14 +1,12 @@
--- 코드를 작성해주세요
--- skill code에서 front end만의 스택을 모아, developers 테이블에 skill code와 일치하는 개발자의 id,email,first_name,last_name 출력(id asc)
-
-with get_front_code_sum as(
-    select sum(s.code) as code_sum
-    from skillcodes as s
-    where category like 'Front%'
+-- 프론트엔드 개발자의 점수 총합을 구한뒤 비트연산
+with get_front_bit as (
+    select sum(s.code) as code
+    from skillcodes s
+    where category like "Front%"
     group by category
 )
 
-select id,email, first_name,last_name
-from developers d ,get_front_code_sum su
-where skill_code & su.code_sum
+select id,email, first_name, last_name
+from developers d
+where skill_code & (select f.code from get_front_bit f)
 order by 1 asc
