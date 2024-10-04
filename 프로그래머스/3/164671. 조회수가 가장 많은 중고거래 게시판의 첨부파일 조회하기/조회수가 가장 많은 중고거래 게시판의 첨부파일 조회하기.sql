af2448d0c1cb8811781e68ed740546b0c2e698aa
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 21:14:05 +0900

    [level 3] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/3/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,10 +1,5 @@
 -- 코드를 입력하세요
-with get_most_view as(
-    select board_id, dense_rank() over(order by ugb.views desc) as rnk
-    from used_goods_board as ugb    
-)
-
-select concat("/home/grep/src/",ugb.board_id,"/",file_id,file_name,file_ext) as file_path
-from used_goods_board as ugb inner join used_goods_file as ugf on ugb.board_id = ugf.board_id
-where ugb.board_id in (select board_id from get_most_view as gmv where gmv.rnk = 1)
-order by file_id desc
+select concat("/home/grep/src/",b.board_id,"/",file_id,file_name, file_ext) as file_path
+from used_goods_board as b inner join used_goods_file f on b.board_id = f.board_id
+where b.views in (select max(views)  from used_goods_board)
+order by 1 desc
