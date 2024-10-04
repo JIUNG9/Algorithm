aa87d0891dcc982c92aa4444680768912b8a8f29
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 20:52:34 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,14 +1,11 @@
-with get_type_max_length as (
-
-    select fish_type, max(length) as length
-    from fish_info
-    group by fish_type 
-
+with get_max as(
+select fish_type, max(length) as length
+from fish_info
+group by fish_type
 )
-, get_type_max as (
-select id, fi.fish_type, fi.length
-from get_type_max_length as ml inner join fish_info as fi on ml.fish_type = fi.fish_type and ml.length = fi.length
-    )
 
-select id, fish_name, length
-from fish_name_info as fni inner join get_type_max as tm on tm.fish_type = fni.fish_type
+
+select id, fish_name,length
+from fish_info fi inner join fish_name_info fni on fi.fish_type = fni.fish_type
+where (fi.fish_type, length) in (select fish_type,length from get_max)
+order by 1 asc
