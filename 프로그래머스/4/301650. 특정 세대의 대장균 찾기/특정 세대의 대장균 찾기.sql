cb394a9619955d0807abaad0d4e211ef8f17f390
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 17:41:50 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,18 +1,18 @@
 -- 코드를 작성해주세요
-with recursive cte as(
-    select id, 1 as gen
-    from ecoli_data
-    where parent_id is null
 
+with recursive cte as(
+-- parent_id가 null인 0 세대 부터 시작하여, 이전에 존재하던 id와 parent_id가 일치하는 것을 찾아 그것을 다음 세대라고 칭하며 이를 3세대까지 구한다. 
+    select id, parent_id, 1 as gen 
+    from ecoli_data 
+    where parent_id is null
+    
     union all
     
-    select child.id, (gen+1) as gen
-    from cte inner join ecoli_data as child on cte.id = child.parent_id
-    where gen < 4
-
+    select e.id, e.parent_id, gen+1
+    from cte as c inner join ecoli_data e on c.id = e.parent_id
+    where gen < 3
 )
-
 select id
 from cte
 where gen = 3
-order by id asc
+order by 1
