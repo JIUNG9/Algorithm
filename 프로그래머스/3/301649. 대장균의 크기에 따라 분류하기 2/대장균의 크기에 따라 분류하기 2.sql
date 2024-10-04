4682e2d7c43b549215617714a567f73edb638da3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-04 19:13:35 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,26 +1,17 @@
-# -- 코드를 작성해주세요
-with get_sum as (
-    select count(*) as total
-    from ecoli_data
-                 )
-,sort_asc_by_size as (
-    select *
-    from ecoli_data
-    order by size_of_colony asc 
-    
+WITH get_rnk AS (
+    SELECT 
+        id, 
+        size_of_colony,
+        NTILE(4) OVER (ORDER BY size_of_colony DESC) as quartile
+    FROM ecoli_data
 )
-                 
-# select *
-# from sort_asc_by_size
-
-select id,
-case
-when total /4 * 3 < row_number() over(order by size_of_colony asc) then 'CRITICAL'
-when total /4 * 2 < row_number() over(order by size_of_colony asc) then 'HIGH'
-when total /4 * 1 < row_number() over(order by size_of_colony asc) then 'MEDIUM'
-when total /4 * 0 < row_number() over(order by size_of_colony asc) then 'LOW'
-end as colony_name
-from ecoli_data, get_sum
-order by 1 asc
-
--- why count(*) doesn't return all of the number of list. -> it is grouped by the id.
+SELECT 
+    id,
+    CASE quartile
+        WHEN 1 THEN 'CRITICAL'
+        WHEN 2 THEN 'HIGH'
+        WHEN 3 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS colony_name
+FROM get_rnk
+ORDER BY id ASC;
