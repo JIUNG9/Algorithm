e8c65931e07a842ba4415abb0df1a14b8545b6e2
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 06:24:52 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 197.48 ms, Memory: 206 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,27 +1,28 @@
 import java.util.*;
+
 class Solution {
-    private Stack<int[]> stk;
     public int[] solution(int[] numbers) {
+                //모든 것을 순회하면서  저장했던 숫자들이 존재하고 현재 확인하는 숫자보다 작다면(현재 숫자가 더 크다면) 이전에 저장했던 idx에 현재 확인 하는 원소로 결과 값을 모두 업데이트한 이후 현재 숫자를 저장한다. 
+
         int len = numbers.length;
-        stk = new Stack<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>((arr1,arr2)->{
+            return Integer.compare(arr1[1],arr2[1]);
+        });
         int[] answer = new int[len];
-        Arrays.fill(answer,-1);
-        stk.push(new int[]{0, numbers[0]});
+        Arrays.fill(answer, -1);
         
-        for(int i = 1; i< len; i++){
+        for (int i = 0; i < len; i++) {
+            int current = numbers[i];
             
-            int currValue = numbers[i];
-            
-            while(!stk.isEmpty()&& stk.peek()[1] < currValue){
-                int[] lastElement = stk.pop();
-                int idx = lastElement[0];
-                int value = lastElement[1];
-                answer[idx] = currValue;
-                
+            while (!pq.isEmpty() && current > pq.peek()[1]) {
+                int[] arr = pq.poll(); 
+                int idx = arr[0];
+                answer[idx] = current; 
             }
-            stk.push(new int[]{i,numbers[i]});
+            
+            pq.add(new int[]{i, current});
         }
-        return answer;
         
+        return answer;
     }
 }
