fb1fe1c4341f42b5ae11ce7b4522335c21c23724
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 08:46:26 +0900

    [level 2] Title: 땅따먹기, Time: 49.19 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,40 +1,31 @@
 class Solution {
     private int answer = -1;
     int solution(int[][] land) {
-        
-        int row = land.length;
-        int[][] dp= new int[row][4];
 
+        //두 번째 행부터 끝 행까지 매 줄마다 자기 자신과 동일한 열을 제외한 이전 줄의 가장 큰 값을 더하여 마지막 행의 가장 큰 값을 반환한다.
+        int len = land.length;
+        int width = land[0].length;
+        int[][] dp = new int[len][width];
+        
         dp[0][0] = land[0][0];
         dp[0][1] = land[0][1];
         dp[0][2] = land[0][2];
         dp[0][3] = land[0][3];
         
-        
-        for(int i = 1; i< row; i++){
-            int[] prevRow = dp[i-1];
-            for(int j = 0; j < prevRow.length; j++){
-                int max = getMaxValueWithoutCurrIdx(j,prevRow);
-                dp[i][j] +=(land[i][j] + max);
+        for(int i = 1; i < len; i++){
+            for(int j = 0; j < width; j++){
+                int max = -1;
+                for(int k = 0; k < width; k++){
+                    if(k==j) continue;
+                    max = Math.max(max, dp[i-1][k]);
+                }
+                dp[i][j] = land[i][j] + max;
             }
         }
         
-        for(int k = 0; k < 4; k++){
-            answer = Math.max(answer,dp[row-1][k]);
+        for(int z = 0; z < 4; z++){
+            answer = Math.max(answer, dp[len-1][z]);
         }
-    
         return answer;
-
-    }
-    
-    private int getMaxValueWithoutCurrIdx(int idx, int[] row){
-        int max = -1;
-        for(int i = 0; i < 4; i++){
-            if(idx == i) continue;
-            max = Math.max(max,row[i]);
-        }
-        return max;
-        
-        
     }
 }
