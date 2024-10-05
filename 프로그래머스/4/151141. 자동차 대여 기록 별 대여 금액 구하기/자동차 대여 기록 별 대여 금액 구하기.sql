3368f8a10dc7926fe5fa2fbca8fbe4385340a6a9
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-05 09:55:44 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,29 +1,42 @@
-WITH CJ AS (
-SELECT  RH.HISTORY_ID
-        ,C.CAR_TYPE
-        ,C.DAILY_FEE
-        ,DATEDIFF(RH.END_DATE,RH.START_DATE)+1 AS DAYS
-  FROM  CAR_RENTAL_COMPANY_RENTAL_HISTORY AS RH
-  JOIN  CAR_RENTAL_COMPANY_CAR AS C
-    ON  RH.CAR_ID = C.CAR_ID
- WHERE  C.CAR_TYPE = '트럭'
-)
-, CAR_RENTAL AS (
-SELECT  CJ.*
-        ,CASE 
-        WHEN DAYS >= 90 THEN 15
-        WHEN DAYS >= 30 THEN 8
-        WHEN DAYS >= 7 THEN 5
-        ELSE 0 END RATE
-  FROM  CJ
-  JOIN  CAR_RENTAL_COMPANY_DISCOUNT_PLAN AS DP
-    ON  CJ.CAR_TYPE = DP.CAR_TYPE
- GROUP
-    BY  CJ.HISTORY_ID
-)
+-- 코드를 입력하세요
+-- company car에서 car_type이 트럭인 car_id만을 가지는 history_id를 얻고 해당 기간에 따른 할인률을 기간 별로 CAR_RENTAL_COMPANY_DISCOUNT_PLAN에서 얻어 history별로 할인을 적용하여(대여일수 * 100 - discountRate / 100) fee를 구한다.
 
-SELECT  HISTORY_ID
-        ,ROUND(DAILY_FEE * DAYS * (1-RATE*0.01),0) AS FEE
-  FROM  CAR_RENTAL
- ORDER
-    BY  FEE DESC, HISTORY_ID DESC
+SELECT 
+    h.history_id, 
+    CASE 
+        -- For rentals of 90 days or more
+        WHEN TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1 >= 90 THEN 
+            ROUND(c.daily_fee * (TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1) * 
+            (100 - (SELECT discount_rate 
+                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                    WHERE DURATION_TYPE = '90일 이상' 
+                    AND car_type = '트럭')) / 100, 0)
+        
+        -- For rentals of 30 to 89 days
+        WHEN TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1 >= 30 THEN 
+            ROUND(c.daily_fee * (TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1) * 
+            (100 - (SELECT discount_rate 
+                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                    WHERE DURATION_TYPE = '30일 이상' 
+                    AND car_type = '트럭')) / 100, 0)
+        
+        -- For rentals of 7 to 29 days
+        WHEN TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1 >= 7 THEN 
+            ROUND(c.daily_fee * (TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1) * 
+            (100 - (SELECT discount_rate 
+                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN 
+                    WHERE DURATION_TYPE = '7일 이상' 
+                    AND car_type = '트럭')) / 100, 0)
+        
+        -- For rentals of less than 7 days (no discount)
+        ELSE ROUND(c.daily_fee * (TO_DAYS(h.end_date) - TO_DAYS(h.start_date) + 1), 0)
+    END AS fee
+FROM 
+    car_rental_company_car c
+INNER JOIN 
+    car_rental_company_rental_history h 
+    ON c.car_id = h.car_id
+WHERE 
+    c.car_type = '트럭'
+ORDER BY 
+    fee DESC, h.history_id DESC;
