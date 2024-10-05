95ac7a24bc3e49a9cbef36e643b085b2e48a933d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 07:55:51 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,17 +1,13 @@
 -- 코드를 입력하세요
--- Impl: 식품을 grouping 하여, 가장 비싼 가격의 분류 가격, 이름의 순위를 얻어(dense rank),<> 식품 분류의 1등 상품의 국, 과자, 김치, 식용유만을 출력하고, 가격을 기준으로 DESC한다.
-
-with get_rnk as (
-
-    select category, price, product_name, dense_rank() over(partition by category order by price desc) as rnk 
+with get_most_expensive as(
+    select product_id, category, dense_rank() over(partition by category order by price desc) as rnk
     from food_product fp
-    where fp.category like '과자' or fp.category like '국' or fp.category like '김치' or fp.category like '식용유'
-
 )
 
+# select *
+# from get_most_expensive
 
-
-select fp.category, fp.price as max_price, fp.product_name
-from food_product as fp inner join get_rnk as gr on fp.product_name = gr.product_name
-where rnk = 1
-order by 2 desc
+select category,price as max_price, product_name
+from food_product fp
+where fp.product_id in (select product_id from get_most_expensive where rnk = 1) and (category like '국' or category like '과자' or category like '김치' or category like '식용유')
+order by max_price desc
