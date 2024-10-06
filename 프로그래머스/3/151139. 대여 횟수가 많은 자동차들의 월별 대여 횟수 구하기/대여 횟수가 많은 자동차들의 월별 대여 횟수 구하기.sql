0025bf88bd7799445b8c984e214e46d35559f897
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 20:16:21 +0900

    [level 3] Title: 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
+++ 프로그래머스/3/151139. 대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기/대여 횟수가 많은 자동차들의 월별 대여 횟수 구하기.sql
@@ -1,26 +1,19 @@
-# with get_rent_over_5 as(
-#     select car_id
-#     from car_rental_company_rental_history
-#     where start_date >= '2022-08-01' and end_date <= '2022-10-31'
-#     group by car_id
-#     having count(car_id) >= 5
-# )
-# -- 8, 9, 10 중에 car_id가 렌트한 흔적이 없다면 해당 car_id 포함하지 않는다, start_month가 3개가 아니라면 포함하지 않는다.
-# select month(start_date) as month, car_id, count(*) as records
-# from car_rental_company_rental_history h
-# where h.car_id in (select * from get_rent_over_5)
-# group by month(start_date)
-# order by month, car_id desc
+-- 도메인 이해: history에는 동일한 car_id가 존재한다. window clause로 데이터를 filter해도 본문 select에서 또한 날짜를 filter 해줘야한다.
+-- 대여 회수가 대여 시작일 기준 2022년 8월부터 10월까지 대여 횟수가 5회 이상인 자동차를 기준으로 해당 월별, 자동차 ID별 대여횟수를 출력하고, 특정 월의 총 대여 횟수가 0인 경우 결과에서 제외 정렬 기준은 월 오름차순, 차 id로 내림차순
+with filter_data as(
+    select car_id
+    from car_rental_company_rental_history h
+    where start_date between '2022-08-01' and '2022-10-31'
+    group by car_id
+    having count(*) >=5
+)
 
-SELECT MONTH, CAR_ID, RECORDS
-FROM (SELECT MONTH(START_DATE) AS MONTH, CAR_ID, COUNT(*) AS RECORDS
-      FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-      WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31'
-      AND CAR_ID IN (SELECT CAR_ID
-                 FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-                 WHERE START_DATE BETWEEN '2022-08-01' AND '2022-10-31'
-                 GROUP BY 1
-                 HAVING COUNT(*) >= 5)
-      GROUP BY 1, 2) AS RENT
-ORDER BY 1, 2 DESC;
 
+
+select month(start_date) as month, h.car_id, count(history_id) as records
+from filter_data f inner join car_rental_company_rental_history h on h.car_id = f.car_id
+where start_date between '2022-08-01' and '2022-10-31'
+group by month, car_id
+
+having count(history_id) != 0
+order by month asc, car_id desc
