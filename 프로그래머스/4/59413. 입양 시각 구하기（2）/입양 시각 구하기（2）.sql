bfec29fae15cea016952deaa1e82f45baef4bc9d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 20:30:15 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,23 +1,14 @@
-
--- domain cte에는 존재하지만 해당 outs에 존재하지 않는 시각이 있을 수 있다. 조인 시 outs에 존재하지 않는 시간도 보여야하므로 cte를 기준으로 join한다.
-
-
+-- 코드를 입력하세요
 with recursive cte as(
-
     select 0 as hour
-    
     union all
     
     select hour + 1
     from cte
     where hour < 23
-    
 )
 
 select hour, count(animal_id) as count
 from cte c left join animal_outs ao on c.hour = hour(ao.datetime)
 group by hour
-# order by hour asc
-
-
-
+order by 1
