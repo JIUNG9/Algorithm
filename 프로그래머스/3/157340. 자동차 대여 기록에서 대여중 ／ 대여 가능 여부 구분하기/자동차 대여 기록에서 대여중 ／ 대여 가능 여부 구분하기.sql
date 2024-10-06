8068cd257c9ef19c6c6d6116b0b1201235b8c1f1
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-06 10:29:44 +0900

    [level 3] Title: 자동차 대여 기록에서 대여중 / 대여 가능 여부 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
+++ 프로그래머스/3/157340. 자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기/자동차 대여 기록에서 대여중 ／ 대여 가능 여부 구분하기.sql
@@ -1,17 +1,21 @@
-WITH rented AS (
-    SELECT car_id
-    FROM car_rental_company_rental_history
-    WHERE '2022-10-16' BETWEEN start_date AND end_date
-        OR end_date = '2022-10-16'
+-- 코드를 입력하세요
+-- 10월 16일에 대여 중이라는 것은 start_date가 10월 16일 보다 작거나 같고, end_date가 10월 16일보다 크거나 같은 것을 의미한다.
+-- 도메인, 히스토리에는 같은 car_id가 여러 개 있을 수 있다. 즉 같은 아이디로 대여가 가능하는 차 목록이라도, 10월 16일에 해당 같은 차 아이디로 대여 기록이 있을 수 있다. 
+with can_not_rent as(
+    select car_id
+    from car_rental_company_rental_history
+    where start_date <= '2022-10-16' and end_date >= '2022-10-16'
 )
 
-SELECT car_id, '대여중' AS AVAILABILITY
-FROM rented
 
-UNION
 
-SELECT  car_id, '대여 가능' AS AVAILABILITY
-FROM car_rental_company_rental_history
-WHERE car_id NOT IN (SELECT car_id FROM rented)
+select car_id, '대여중' as availability
+from can_not_rent
 
-ORDER BY car_id DESC;
+union all
+
+select distinct car_id , '대여 가능' as availability
+from car_rental_company_rental_history
+where car_id not in (select car_id from can_not_rent)
+order by car_id desc
+
