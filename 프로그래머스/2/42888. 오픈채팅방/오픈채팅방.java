96270686d1b425f43c3be55ce984798a43bc328d
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-11 10:01:20 +0900

    [level 2] Title: 오픈채팅방, Time: 205.77 ms, Memory: 165 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,50 +1,57 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
-    
-    private Map<String,String> map;
-    private List<String> list;
-    
     public String[] solution(String[] record) {
-        map = new HashMap<>();
-        list = new ArrayList<>();
-        String[][] str = new String[record.length][3];
-        int idx = 0;
-        //split by " "
-        for(String s: record){
-            str[idx++] = s.split(" ");    
+        
+        //최종적으로 업데이트 된 이름으로 순차적으로 greeting 메시지와 exit message가 출력돼야한다. Change 메시지는 출력하지 않는다. Change일 경우 닉네임만 수정한다. 나가는 경우는 
+        //HashMap (아이디, 닉네임) // 순서 -> Queue(명령어, id)
+        //닉네임만 수정된다는 것은 map의 값만을 수정하면 된다는 것, 메시지 출력x 
+        Queue<String[]> q = new LinkedList<>(); 
+        Map<String, String> map = new HashMap<>();
+        List<String> answer = new ArrayList<>();
+        for(int i = 0; i < record.length; i++){
+            String[] arr = record[i].split(" ");
+            String command = "";
+            String id = "";
+            String nickname = "";
+            
+            if(arr.length < 3){
+                 command = arr[0];    
+                 id = arr[1];
+            }
+            
+            else{
+                 command = arr[0];  
+                 id = arr[1];
+                 nickname = arr[2];    
+            }
+                        
+            if(!nickname.equals("")) map.put(id, nickname);
+            if(!command.equals("Change")) q.add(new String[]{command, id});
+
         }
         
-        
-        //iterate for getting final nickname
-        
-        for(String[] sArr : str){
-            String type = sArr[0];
-            if(type.equals("Enter") || type.equals("Change")){
-                map.put(sArr[1], sArr[2]);    
-            }
+        while(!q.isEmpty()){
+            String[] arr = q.poll();
+            String command = arr[0];
+            String id = arr[1];
+            answer.add(getMessage(map.get(id),command.substring(0,1)));
         }
+        return answer.stream().toArray(String[]::new);
         
         
-        //get answer (when str[0] == enter || str[0] == leave)
-        for(String[] sArr : str){
-            String type = sArr[0];
-            String nickname = map.get(sArr[1]);
-            if(type.equals("Enter")){
-                list.add(getGreetingMessage(nickname));
-            }
-            if(type.equals("Leave")){
-    
-                list.add(getLeaveMessage(nickname));    
-            }
-    }
-        
-        return list.toArray(String[]::new);
     }
     
-    public String getGreetingMessage(String nickname){
-        return nickname + "님이 들어왔습니다.";
+    public String getMessage(String name, String command){
+        if(command.equals("E")){
+            return name +"님이 들어왔습니다.";
+        
+        }
+        else if(command.equals("L")){
+            return name + "님이 나갔습니다.";
+        }
+        return "";
+    
     }
-    public String getLeaveMessage(String nickname){
-        return nickname + "님이 나갔습니다.";
-    }
+        
 }
