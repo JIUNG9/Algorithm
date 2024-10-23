32acf3cb5d22fed7a0e99124a8b08d3ff4c2f276
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-23 09:36:01 +0900

    [level 3] Title: 입국심사, Time: 131.56 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,48 +1,30 @@
 import java.util.*;
-import java.math.BigDecimal;
-
 class Solution {
-    private long answer;
     public long solution(int n, int[] times) {
-        answer = Long.MAX_VALUE;
-        //먼저 고객을 처리하는 심사관을 우선적으로 처리하도록 하여 최적의 결과값을 얻는다.-> 최적:(그리디)
-        //n을 조정하여 순회할 수 없다 -> input 값이 10billion
-        //시각을 통하여 (Parameterized binary search를 사용해야한다.)
-        //특정 시각을 지정하여 특정 시각을 모두 체크할 때 까지, 해당 시각에 모든 요청을 처리할 수 있으면(해당 시각으로 심사관이 처리할 수 있는 인원 수의 합이 n보다 같거나 커야한다.) 더 적은 시각으로 해당 요청을 처리하고 아니라면 더 많은 시각으로 요청을 처리한다. 요청을 처리할 수 있을 때 answer와 비교하여 더 작은 값으로 업데이트한다.
-        //int left = 0; int right = 가장 오래걸리는 심사관이 모든 일을 처리; mid = (left + right)
+        //1부터 가장 큰 값이 모두 처리하는 시간까지
+        //모든 시간을 처리할 수 없다면 끝 지점 + 현재 지점 / 2, 만약 모두 처리할 수 있다면 현재지점을 끝지점으로 반환, 만약 모두 처리할 수 있다면 정답 반환.
         
-        //이후 가장 작은 값을 정답으로 리턴
         Arrays.sort(times);
-        int len = times.length;
         long left = 0;
-        long right = ((long)times[len/2] * n);
-        
-        
-        
-        while(left <= right){
-            long mid = (left+right)/2;
-            long thePersonNumberWhichIsHandled = getTimeTaken(times,mid);
-            if(n <= thePersonNumberWhichIsHandled){
-                if(answer > mid) answer = mid;
-                right = mid -1 ;
+        long right = 1000000000000000000L;
+        long sum = 0;
+        long mid = (left + right)/2;
+        while(left < right){
+            sum = 0;
+            mid = (left + right) / 2;
+            for(int i = 0; i < times.length; i++){
+                sum+=(mid / times[i]);
             }
-            else{
+            if(sum >= n){
+                right = mid;
+            }
+            else if(sum < n){
                 left = mid + 1;
             }
-        }
-        
-        return answer;
-    }
-            
-        
-    public long getTimeTaken(int[] times, long mid){
-        long sum = 0;
-        for(int i = 0; i < times.length; i++){
-            sum+=(mid / times[i]);
-        }
-        
-        return sum;
-        
+                
+        }  
+        return left;
     
+        
     }
 }
