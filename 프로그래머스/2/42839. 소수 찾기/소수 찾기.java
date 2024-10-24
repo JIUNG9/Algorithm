2c1468fc10f580ecf2865f3ce316d6f0d9db3954
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-24 19:33:07 +0900

    [level 2] Title: 소수 찾기, Time: 15.05 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,49 @@
 import java.util.*;
-
 class Solution {
+    private boolean[] visited;
     private Set<Integer> set;
- public static boolean isPrime(int num) {
-    if (num == 1) {
-      return false;
+    public int solution(String numbers) {
+        //주어진 numbers를 한 번씩 사용하여 만들 수 있는 모든 숫자의 경우를 구하여 구한 것들 중, 만약 소수라면 해당 숫자를 정답에 추가한다. 
+        String[] arr = numbers.split("");
+        set = new HashSet<>();
+        int len = arr.length;
+        visited = new boolean[len];
+        dfs("",arr);
+        return theNumberOfPrime(set);
+        
+        
     }
-    if (num == 2) {
-      return true;
+    public int theNumberOfPrime(Set<Integer> set){
+        int answer =0; 
+        for(Integer i : set)
+        {
+            boolean flag = true;
+            if(i == 2) {
+                answer++; continue;
+            }
+            if(i == 0 || i == 1) continue;
+            if(i % 2 == 0) continue;
+            
+            for(int j = 3; j <= Math.sqrt(i); j+=2){
+                
+                if(i % j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) answer++;
+        }        
+        return answer;
+        
     }
-    if (num % 2 == 0) {
-      return false;
+    public void dfs(String curr, String[] arr){
+        if(!curr.equals("")) set.add(Integer.parseInt(curr));
+        for(int i = 0; i < arr.length; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                dfs(curr+arr[i], arr);                
+                visited[i] = false;
+            }
+        }
     }
-
-    for (int i = 3; i <= Math.sqrt(num); i += 2) {
-      if (num % i == 0) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public int solution(String numbers) {
-    set = new HashSet<>();
-    int len = numbers.length();
-    boolean[] visited = new boolean[len];
-    dfs(visited, numbers, "");
-    int answer = (int) set.stream().filter(Solution::isPrime).count();
-    return answer;
-  }
-
-  public void dfs(boolean[] visited, String elements, String curr) {
-    for (int i = 0; i < elements.length(); i++) {
-      if (!visited[i]) {
-        visited[i] = true;
-        set.add(Integer.parseInt(curr + elements.charAt(i)));
-        dfs(visited, elements, curr + elements.charAt(i));
-        visited[i] = false;
-      }
-    }
-  }
-
-    
 }
