2053f63ff60963148923ba823d5fc47eea605ad0
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-24 20:27:58 +0900

    [level 1] Title: 모의고사, Time: 1.54 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,50 +1,37 @@
 import java.util.*;
-import java.util.stream.*;
+
 class Solution {
- 
+    private int[] a = {1,2,3,4,5};
+    private int[] b = {2,1,2,3,2,4,2,5};
+    private int[] c = {3,3,1,1,2,2,4,4,5,5};
+    private int aCounter = 0;
+    private int bCounter = 0;
+    private int cCounter = 0;
     
-    private int[] firstMan = {1,2,3,4,5};
-    private int[] secondMan = {2,1,2,3,2,4,2,5};
-    private int[] thirdMan = {3,3,1,1,2,2,4,4,5,5};
-    private List<Integer> answerList;
-    int firstManCounter = 0;
-    int secondManCounter = 0;
-    int thirdManCounter = 0;
     public int[] solution(int[] answers) {
-        
-        int firstManLen = firstMan.length;
-        int secondManLen = secondMan.length;
-        int thirdManLen = thirdMan.length;
-        answerList = new ArrayList<>();
-        
+        //각 수포자의 규칙을 구하여, 해당 규칙에 맞는 정답의 개수를 각각 구하여 가장 정답을 많이 맞춘 사람의 번호를 반환한다. 이때 여러명이라면 모두 담고 오름차순 정렬한다.
+        List<int[]> list = new ArrayList<>();
         for(int i = 0; i < answers.length; i++){
-            int answer = answers[i];
-            if(answer == firstMan[i%firstManLen]) firstManCounter++;
-            if(answer == secondMan[i%secondManLen]) secondManCounter++;
-            if(answer == thirdMan[i%thirdManLen]) thirdManCounter++;
+            if(answers[i] == a[i%5]) aCounter++;
+            if(answers[i] == b[i%8]) bCounter++;
+            if(answers[i] == c[i%10]) cCounter++;
         }
+        list.add(new int[]{1,aCounter});
+        list.add(new int[]{2,bCounter});
+        list.add(new int[]{3,cCounter});
         
-        List<List<Integer>> counterList = List.of(List.of(1,firstManCounter),List.of(2,secondManCounter),List.of(3,thirdManCounter));
-                
-        counterList = counterList.stream().sorted((list1,list2)->{
-            int answer = Integer.compare(list2.get(1),list1.get(1));
-            if(answer == 0) return Integer.compare(list1.get(0),list2.get(0));
-            return answer;
-        }).collect(Collectors.toList());
+        list.sort((arr1,arr2)->{
+            return Integer.compare(arr2[1], arr1[1]);
+        });
         
-        int maxValue = counterList.get(0).get(1);
-        for(List<Integer> list : counterList){
-            int value = list.get(1);
-            int manIdx = list.get(0);
-            if(value==maxValue) answerList.add(manIdx);
-        }
-        
-        return answerList.stream().mapToInt(i->i).toArray();
+        if(list.get(0)[1] == list.get(1)[1] && list.get(1)[1] == list.get(2)[1]) return new int[]{1,2,3};
+        if(list.get(0)[1] == list.get(1)[1]) return new int[]{list.get(0)[0], list.get(1)[0]};
+        else{
+          return new int[]{list.get(0)[0]};
+        } 
         
         
         
-        
-        
-        
+
     }
 }
