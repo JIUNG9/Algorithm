ce0891f8c7f90300d07ce00798cb2be9a0e6cd5f
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2024-10-26 20:05:29 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.46 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,46 @@
 import java.util.*;
 class Solution {
-    private int[][] move = {{1,0},{0,1}, {-1,0},{0,-1}};
+    int[][] move = {{1,0},{-1,0},{0,-1},{0,1}};
+    boolean[][] visited;
     public int solution(int[][] maps) {
-        //게임 맵을 초과하지 않고, 벽을 넘어서지 않으며(벽에는 갈 수 없음) 가장 최단 거리로 map의 끝지점에 도달하는 거리를 구하시시오. 
+        //맵을 넘어서지 않고, 이미 방문한 곳을 방문하지 않고, 벽이 아닌 곳을 방문할 때 가장 빠르게 도착하는 방법, 가장 빠르게 도착했을 때, 진행한 횟수를 반환
         
-        //Queue를 통해 이전의 방문했던 것을 다시 한 번 방문하지 않고 맵의 크기를 체크하고, 벽인지 아닌지 체크하며 방문, 방문한 것은 방문처리를하고, 현재까지 방문한 횟수를 기록. 즉 움직인 위치와 현재까지 움직인 거리를 기록해야함. 이때 캐릭터가 map의 끝지점의 다달았을 때 해당 움직인 거리를 반환. 시작 위치는 0,0 이며 시작 시 1의 거리를 움직였다고 판단, 다다를 수 없을 떄는, Q가 더 이상 모든 곳을 방문해 갈 수 없을 때임.(q의 사이즈가 0)
-        
+        Queue<int[]> q = new LinkedList<>();
         int len = maps.length;
         int width = maps[0].length;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[len][width];
-        q.add(new int[]{0,0,1});
+        visited = new boolean[len][width];
         visited[0][0] = true;
+        
+        q.add(new int[]{0,0,1});
+        
         
         while(!q.isEmpty()){
             int[] arr = q.poll();
-            int currY = arr[0];
-            int currX = arr[1];
+            int y = arr[0];
+            int x = arr[1];
             int counter = arr[2];
-            if(currY  == len -1  && currX == width - 1) return counter;
+            
+            if(x == width -1 && y == len -1) return counter;
             
             for(int[] m : move){
-                int movedY = m[0] + currY;
-                int movedX = m[1] + currX;
-                if(canGo(movedY,movedX, maps,visited)){
-                    visited[movedY][movedX] = true;
-                    q.add(new int[]{movedY,movedX, counter+1});
+                int moveY = y + m[0];
+                int moveX= x + m[1];
+                if(canGo(moveY, moveX, len, width,maps)){
+                    visited[moveY][moveX] = true;
+                    q.add(new int[]{moveY,moveX,counter+1});
                 }
             }
-        }
-        return -1;
             
+            
+            
+        }
+        
+        return -1;
+        
     }
-    //장애물, 방문처리, 맵 사이즈
-    public boolean canGo(int y, int x, int[][] maps , boolean[][] visited){
-        if(y < 0 || x < 0 || x >= maps[0].length || y >= maps.length || visited[y][x] == true || maps[y][x] == 0) return false;
-            return true;
+    
+    public boolean canGo(int currentY , int currentX, int mapY, int mapX, int[][] maps){
+        if(currentX >= 0 && currentY >= 0 && currentX < mapX && currentY < mapY && !visited[currentY][currentX] && maps[currentY][currentX]!=0) return true;
+        return false;
     }
 }
