b1cc5854db8bc71a0db9a4f1543dfa95e75f51d9
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-02-17 20:35:44 +0900

    [level 3] Title: 입국심사, Time: 87.22 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,30 +1,23 @@
-import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        //1부터 가장 큰 값이 모두 처리하는 시간까지
-        //모든 시간을 처리할 수 없다면 끝 지점 + 현재 지점 / 2, 만약 모두 처리할 수 있다면 현재지점을 끝지점으로 반환, 만약 모두 처리할 수 있다면 정답 반환.
+        long left = 0L;
+        long right = 1000000000L * 1000000000L;
+        long answer =  Long.MAX_VALUE;
         
-        Arrays.sort(times);
-        long left = 0;
-        long right = 1000000000000000000L;
-        long sum = 0;
-        long mid = (left + right)/2;
-        while(left < right){
-            sum = 0;
-            mid = (left + right) / 2;
-            for(int i = 0; i < times.length; i++){
-                sum+=(mid / times[i]);
+        while(left <= right){
+            long mid = (left + right) / 2;
+            long sum = 0L;
+            for(Integer t : times){
+                sum+=(mid / (long)t);
             }
             if(sum >= n){
-                right = mid;
+                answer = Math.min(mid,answer);
+                right = mid - 1;
             }
-            else if(sum < n){
+            else{
                 left = mid + 1;
             }
-                
-        }  
-        return left;
-    
-        
+        }        
+        return answer;
     }
 }
