ea6570c38e064468e66a766fa36271f9fbfd93a3
JIUNG GU <60885635+JIUNG9@users.noreply.github.com>
2025-02-18 18:17:04 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,32 +1,19 @@
-WITH RECURSIVE cte AS (
-    SELECT 
-        e1.id, 
-        1 AS gen
-    FROM 
-        ecoli_data AS e1 
-      
-    WHERE 
-        e1.parent_id IS NULL
+-- 각 세대 별, 대장균 ID를 구하고, 해당 데이터의 id를 기준으로 parent_id를 조인하여, 해당 id가 존재하지 않는(right join, parent_id), 해당 아이디를 구한여, 해당 세대의 개수를 세대 별로 구한다. 
 
-    UNION ALL
-
-    SELECT 
-        e1.id, 
-        (cte.gen + 1) AS gen
-    FROM 
-        ecoli_data AS e1 
-        INNER JOIN cte ON cte.id = e1.parent_id
-)
-, get_has_child as (
-    SELECT 
-    e.parent_id
-    From cte  inner join ecoli_data as e on e.parent_id = cte.id
-
-)
-
-select count(*) as count, gen as GENERATION
-from cte
-where id not in (select parent_id as id from get_has_child)
-group by gen
-order by gen asc
+with recursive cte as (
+    select id, 1 as gen
+    from ecoli_data
+    where parent_id is null 
     
+    union all
+    
+    select e.id, gen+1 as gen
+    from cte inner join ecoli_data e on e.parent_id = cte.id
+    where gen < 10
+)
+
+select count(c.id) as count, gen as generation
+from cte c left join ecoli_data e on c.id = e.parent_id
+where e.id is null
+group by 2
+order by 2 asc
